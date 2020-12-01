97ed68389107e6e9f999157bebd6d6da8dcfc14b
Erik Haugan <209444+nnerik@users.noreply.github.com>
2020-12-01 11:23:38 +0100

    erik: Day 01

---
+++ erik/python/day01.py
@@ -0,0 +1,18 @@
+with open("../input/input01a.txt", "r") as input:
+    numbers = [int(line) for line in input.readlines()]
+
+total = 2020
+
+# Part 1
+print("Part One:")
+matches = [n for n in numbers if total - n in numbers]
+print(matches, matches[0] * matches[1])
+
+# Part 2
+print("Part Two:")
+result2 = None
+for m in numbers:
+    matches = [n for n in numbers if total - m - n in numbers]
+    if matches:
+        print([m] + matches, m * matches[0] * matches[1])
+        break
