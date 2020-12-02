f86cbfa310c8e64a84f55072b45af5ad9b784dd8
Stein Magnus Jodal <stein.magnus.jodal@kolonial.no>
2020-12-02 08:47:00 +0100

    jodal: Hide IO inside helper functions

--- jodal/aoc-rs/src/lib.rs
+++ jodal/aoc-rs/src/lib.rs
@@ -1,16 +1,20 @@
 pub mod day01a;
 pub mod day01b;
+pub mod day02a;
 
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+pub fn read_lines<P>(filename: P) -> io::Result<Vec<String>>
 where
     P: AsRef<Path>,
 {
     let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+    Ok(io::BufReader::new(file)
+        .lines()
+        .map(|l| l.unwrap())
+        .collect())
 }
 
 pub fn read_numbers<P>(filename: P) -> io::Result<Vec<i32>>
@@ -18,6 +22,7 @@ where
     P: AsRef<Path>,
 {
     Ok(read_lines(filename)?
-        .map(|line| line.unwrap().parse().expect("Failed parsing number"))
+        .iter()
+        .map(|line| line.parse().expect("Failed parsing number"))
         .collect())
 }
