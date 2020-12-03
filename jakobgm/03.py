2a30a706be8d747ca2072c55244688528d9d5909
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2020-12-03 07:31:32 +0100

    Add solution to problem 03b in python

--- jakobgm/03.py
+++ jakobgm/03.py
@@ -9,12 +9,20 @@ if __name__ == "__main__":
     matrix = []
     for row in rows:
         matrix.append([values[column] for column in row])
-    position = [0, 0]
-    trees = 0
-    num_rows = len(matrix)
-    num_columns = len(matrix[0])
-    while position[0] < num_rows - 1:
-        position[0] += 1
-        position[1] += 3
-        trees += matrix[position[0]][position[1] % num_columns]
-    print(trees)
+
+    def solve(step):
+        position = [0, 0]
+        trees = 0
+        num_rows = len(matrix)
+        num_columns = len(matrix[0])
+        while position[0] < num_rows - 1:
+            position[0] += step[0]
+            position[1] += step[1]
+            trees += matrix[position[0]][position[1] % num_columns]
+        return trees
+
+    print("03a:", solve(step=(1, 3)))
+
+    steps = [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)]
+    costs = [solve(step) for step in steps]
+    print("03b:", costs[0] * costs[1] * costs[2] * costs[3] * costs[4])
