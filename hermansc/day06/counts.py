ae0687fc23de21898f16694a9bb5def34b148dd6
Herman S <hermansc@users.noreply.github.com>
2020-12-06 12:54:33 +0100

    Don't need a lambda for intersection

--- hermansc/day06/counts.py
+++ hermansc/day06/counts.py
@@ -4,5 +4,4 @@ groups = [l.split("\n") for l in open("forms.txt").read().split("\n\n")]
 print(sum([len(set([c for line in g for c in line])) for g in groups]))
 
 # Part 2
-seen = lambda args: set.intersection(*map(set, args))
-print(sum([len(seen([line for line in g])) for g in groups]))
+print(sum([len(set.intersection(*[set(line) for line in g])) for g in groups]))
