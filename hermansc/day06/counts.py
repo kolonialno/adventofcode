c1cf721aa1e194ff0d8b388b1d100f5a0761d329
Herman S <hermansc@users.noreply.github.com>
2020-12-06 18:39:25 +0100

    Use `set.union` and fix grouping logic

--- hermansc/day06/counts.py
+++ hermansc/day06/counts.py
@@ -1,7 +1,20 @@
-groups = [l.split("\n") for l in open("forms.txt").read().split("\n\n")]
+# Make a list of form submissions from each group. One single form is represented as a set.
+# [
+#     [
+#         {a, b, c}, # One form submission
+#     ],
+#     [
+#         {a}, {b}, {c} # Three forms submissions in this group
+#     ]
+# ]
+groups = [[set(c) for c in l.rstrip().split("\n")] for l in open("forms.txt").read().split("\n\n")]
 
 # Part 1
-print(sum([len(set([c for line in g for c in line])) for g in groups]))
+# Within a group we take the union of all submissions and thus get the unique answers
+# for this group.
+print(sum(len(set.union(*g)) for g in groups))
 
 # Part 2
-print(sum([len(set.intersection(*[set(line) for line in g])) for g in groups]))
+# Within a group we take the intersection and thus get the answers within the group shared
+# between every submission (i.e. the intersection :-))
+print(sum(len(set.intersection(*g)) for g in groups))
