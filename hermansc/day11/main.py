6b085de0a093de04d45fe7a3a21e06cb57af8ba0
Herman S <hermansc@users.noreply.github.com>
2020-12-11 17:49:31 +0100

    Some optimizations to todays solution

--- hermansc/day11/main.py
+++ hermansc/day11/main.py
@@ -3,40 +3,33 @@ from copy import deepcopy
 
 layout = [list(line) for line in open("seats.txt").read().strip().split("\n")]
 
-
 def evolve(layout, ridiculous=False):
-    def in_bounds(row, column, max_row, max_column):
-        return not (ridx + ar < 0 or cidx + ac < 0 or ridx + ar >= max_row or cidx + ac >= max_column)
-
-    new_layout = deepcopy(layout)
-    for ridx in range(len(new_layout)):
-        row = new_layout[ridx]
-        for cidx in range(len(row)):
-            seat = new_layout[ridx][cidx]
-
+    new_layout = []
+    for ridx in range(len(layout)):
+        new_row = []
+        for cidx in range(len(layout[0])):
             adjecent = []
-            directions = [c for c in product(range(-1, 2), repeat=2) if c != (0, 0)]
-            for mask in directions:
+            for mask in (c for c in product(range(-1, 2), repeat=2) if c != (0, 0)):
                 scope = 1
-                ar, ac = (mask[0] * scope, mask[1] * scope)
-                while in_bounds(ridx + ar, cidx + ac, max_row=len(layout), max_column=len(row)):
-
-                    if layout[ridx + ar][cidx + ac] != ".":
-                        adjecent.append(layout[ridx + ar][cidx + ac])
+                while ridx + mask[0] * scope in range(len(layout)) and cidx + mask[1] * scope in range(len(layout[0])):
+                    val = layout[ridx + mask[0] * scope][cidx + mask[1] * scope]
+                    if val != ".":
+                        adjecent.append(val)
                         break
 
                     if not ridiculous:
                         break
-
                     scope += 1
-                    ar, ac = mask[0] * scope, mask[1] * scope
 
             lim = 5 if ridiculous else 4
-            if seat == "L" and all(s == "L" for s in adjecent):
-                new_layout[ridx][cidx] = "#"
-            elif seat == "#" and len([s for s in adjecent if s == "#"]) >= lim:
-                new_layout[ridx][cidx] = "L"
+            if layout[ridx][cidx] == "L" and all(s == "L" for s in adjecent):
+                new_row.append("#")
+            elif layout[ridx][cidx] == "#" and len([s for s in adjecent if s == "#"]) >= lim:
+                new_row.append("L")
+            else:
+                new_row.append(layout[ridx][cidx])
 
+        new_layout.append(new_row)
     return new_layout
 
 def find_stable(layout, ridiculous=False):
