bd356a24906706adc50628a5cddf8981cbdbabed
Herman S <hermansc@users.noreply.github.com>
2020-12-14 12:17:10 +0100

    Combine part 1 and 2

--- hermansc/day14/main.py
+++ hermansc/day14/main.py
@@ -4,23 +4,24 @@ from itertools import product
 lines = open("init.txt").read().strip().split("\n")
 
 mask = lines[0].split(" = ")[1]
-mem = {}
+mem1, mem2 = {}, {}
 for line in lines[1:]:
     if line.startswith("mask"):
         mask = line.split(" = ")[1]
         continue
 
     m = re.match("mem\[(\d+)\] = (\d+)", line)
-    address, value = list("{0:b}".format(int(m.group(1))).zfill(36)), int(m.group(2))
 
     # Part 1
-    # for idx, v in enumerate(mask):
-    #   if v == "X":
-    #       continue
-    #   value[idx] = v
-    # mem[address] = int("".join(value), 2)
+    address, value = int(m.group(1)), list("{0:b}".format(int(m.group(2))).zfill(36))
+    for idx, v in enumerate(mask):
+      if v == "X":
+          continue
+      value[idx] = v
+    mem1[address] = int("".join(value), 2)
 
     # Part 2
+    address, value = list("{0:b}".format(int(m.group(1))).zfill(36)), int(m.group(2))
     for combo in list(product([0, 1], repeat=sum(1 for v in mask if v == "X"))):
         c = 0
         for idx, v in enumerate(mask):
@@ -30,6 +31,7 @@ for line in lines[1:]:
             elif v == "1":
                 address[idx] = v
 
-        mem[int("".join(address), 2)] = value
+        mem2[int("".join(address), 2)] = value
 
-print(sum(v for v in mem.values()))
+print(sum(mem1.values()))
+print(sum(mem2.values()))
