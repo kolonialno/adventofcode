f9507d8153e71294da2483e82cd5875d7094936a
Herman S <hermansc@users.noreply.github.com>
2020-12-15 08:45:34 +0100

    Add solution for December 15th (Python)

---
+++ hermansc/day15/main.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+
+numbers = [18,8,0,5,4,1,20]
+records = defaultdict(list, {i: [idx] for idx, i in enumerate(numbers, 1)})
+last_number = numbers[-1]
+c = len(numbers) + 1
+
+# Part 1
+# while c <= 2020:
+
+# Part 2 (lol?)
+while c <= 30000000:
+    idxs = records[last_number]
+    last_number = 0 if len(idxs) == 1 else idxs[-1] - idxs[-2]
+    records[last_number].append(c)
+    c+= 1
+
+print(last_number)
