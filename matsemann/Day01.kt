a9fab0c200b573acb08ed8bda6ce0063ff79c07a
Mats Krüger Svensson <mats@matsemann.com>
2021-12-01 07:05:10 +0100

    mats day 1, kotlin stdlib does it all

---
+++ matsemann/Day01.kt
@@ -0,0 +1,30 @@
+package com.matsemann.adventofcode2021
+
+fun main() {
+    run("1", fileName = "day01_1") { lines ->
+        lines
+            .map { it.toInt() }
+            .zipWithNext { a, b -> a < b }
+            .count { it }
+    }
+    run("2", fileName = "day01_1") { lines ->
+        lines
+            .map { it.toInt() }
+            .windowed(3) { it.sum() }
+            .zipWithNext { a, b -> a < b }
+            .count { it }
+    }
+}
+
+/*
+
+OUTPUT
+======
+
+Done. Took 21ms to run
+Result for 1:	1462
+
+Done. Took 24ms to run
+Result for 2:	1497
+
+ */
---
+++ matsemann/util.kt
@@ -0,0 +1,24 @@
+package com.matsemann.adventofcode2021
+
+import java.nio.file.Files
+import java.nio.file.Path
+
+
+fun getFileLines(fileName: String) =
+    Files.readAllLines(Path.of("adventofcode2021/inputs/$fileName"))
+
+fun run(runName: String? = null, fileName: String, func: (List<String>) -> Any) {
+    val lines = getFileLines("$fileName.txt")
+    val result = measure { func(lines) }
+
+    println("Result for ${runName ?: fileName}:\t${result}\n")
+}
+
+fun <T> measure(func: () -> T) : T {
+    val start = System.currentTimeMillis()
+    val result = func()
+    val time = System.currentTimeMillis() - start
+
+    println("Done. Took ${time}ms to run")
+    return result
+}
