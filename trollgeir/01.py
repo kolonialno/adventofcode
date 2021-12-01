8744924a6c9338f74026962cb4f50480f50f790c
Asgeir Berland <asgeir.berland@oda.com>
2021-12-01 23:55:43 +0100

    pandas vectorization

---
+++ trollgeir/01.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+data = pd.read_csv("input.txt", header=None)[0]
+
+# Puzzle 1
+answer1 = (data.shift(-1) > data).sum()
+
+# Puzzle 2
+rolled = data.rolling(window=3).sum().dropna()
+answer2 = (rolled.shift(-1) > rolled).sum()
+
+print(f"Answer1: {answer1}")
+print(f"Answer2: {answer2}")
