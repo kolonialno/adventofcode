5af2b4e7370b60844d8bcd96f0ea5c436ec275ad
Erik Haugan <209444+nnerik@users.noreply.github.com>
2021-12-01 20:42:03 +0100

    Polish some more

--- nnerik/01.py
+++ nnerik/01.py
@@ -1,22 +1,17 @@
-from pathlib import Path
-
-INPUT_DIR = Path(__file__).parent / "input"
-
-
 def read_input(filename):
-    with open(INPUT_DIR / filename) as f:
+    with open(filename) as f:
         return [int(line) for line in f.readlines()]
 
 
-def solve_part1(input):
+def part1(input):
     return sum(a < b for a, b in zip(input, input[1:]))
 
 
-def solve_part2(input):
-    return solve_part1([a + b + c for a, b, c in zip(input, input[1:], input[2:])])
+def part2(input):
+    return part1([a + b + c for a, b, c in zip(input, input[1:], input[2:])])
 
 
 if __name__ == "__main__":
-    input = read_input("day01.txt")
-    print(f"Part 1: {solve_part1(input)}")
-    print(f"Part 2: {solve_part2(input)}")
+    input = read_input(__file__[:-3] + ".txt")
+    print(f"Part 1: {part1(input)}")
+    print(f"Part 2: {part2(input)}")
