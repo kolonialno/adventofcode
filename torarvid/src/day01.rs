24f2489e4cf527c6e5f91e4259393c7f7dfed7e1
Tor Arvid Lund <tor.arvid.lund@oda.com>
2021-12-01 11:39:31 +0100

    torarvid: day01 (rust, steal some of @paalel's clever tricks)

--- torarvid/src/day01.rs
+++ torarvid/src/day01.rs
@@ -2,24 +2,14 @@ use crate::util::file_by_lines_as_i32;
 
 pub fn run() {
     let numbers = file_by_lines_as_i32("day01.txt");
-    let mut count = 0;
-    for i in 1..numbers.len() {
-        if numbers[i] > numbers[i - 1] {
-            count += 1;
-        }
-    }
-    println!("Part 1: {}", count);
+
+    let part1 = numbers.windows(2).filter(|w| w[0] < w[1]).count();
+    println!("Part 1: {}", part1);
 
     // Part 2
-    let mut group_sum: Option<i32> = None;
-    count = 0;
-    for i in 2..numbers.len() {
-        let new_group_sum = numbers[i - 2] + numbers[i - 1] + numbers[i];
-        if group_sum.is_some() && group_sum.unwrap() < new_group_sum {
-            count += 1;
-        }
-        group_sum = Some(new_group_sum);
-    }
-    println!("Part 2: {}", count);
+    let grouped: Vec<_> = numbers.windows(3).map(|w| w[0] + w[1] + w[2]).collect();
+
+    let part2 = grouped.windows(2).filter(|w| w[0] < w[1]).count();
+    println!("Part 2: {}", part2);
 }
 
