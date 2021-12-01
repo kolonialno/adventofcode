93410eacc16b07a6306c007d3706814c982abaa5
Erik Haugan <209444+nnerik@users.noreply.github.com>
2021-12-01 20:21:21 +0100

    Polish

--- nnerik/01.py
+++ nnerik/01.py
@@ -7,12 +7,14 @@ def read_input(filename):
     with open(INPUT_DIR / filename) as f:
         return [int(line) for line in f.readlines()]
 
+
 def solve_part1(input):
     return sum(a < b for a, b in zip(input, input[1:]))
 
+
 def solve_part2(input):
-    series = [a + b + c for a, b, c in zip(input, input[1:], input[2:])]
-    return sum(a < b for a, b in zip(series, series[1:]))
+    return solve_part1([a + b + c for a, b, c in zip(input, input[1:], input[2:])])
+
 
 if __name__ == "__main__":
     input = read_input("day01.txt")
