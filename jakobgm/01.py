ebf0f039e513d7108640c5cb5e388c1b45864b7b
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2021-12-01 08:38:34 +0100

    Clean up solution to day 1 somewhat
    More comprehensions

--- jakobgm/01.py
+++ jakobgm/01.py
@@ -1,22 +1,22 @@
 from pathlib import Path
 
-p = Path("input.txt")
-measures = p.read_text().strip().split("\n")
-measures = [int(x) for x in measures]
+# Input parsing
+p = Path("inputs/01.txt")
+measures = tuple(int(digit) for digit in p.read_text().strip().split("\n"))
 
+# Task a
 increasing = 0
-for previous, this in zip(measures[:-1], measures[1:]):
-    if previous < this:
+for previous, current in zip(measures[:-1], measures[1:]):
+    if previous < current:
         increasing += 1
 print(increasing)
 
-windows = []
-for a, b, c in zip(measures[:-2], measures[1:-1], measures[2:]):
-    windows.append(a + b + c)
-
-increasing = 0
+# Task b
+windows = tuple(
+    a + b + c for a, b, c in zip(measures[:-2], measures[1:-1], measures[2:])
+)
+increasing_windows = 0
 for previous, this in zip(windows[:-1], windows[1:]):
     if previous < this:
-        increasing += 1
-
-print(increasing)
+        increasing_windows += 1
+print(increasing_windows)
