80da944b4e77a4186d3859b399614979c1e46529
Pål Erik Lystad <pal.erik.lystad@oda.com>
2021-12-02 10:41:20 +0100

    Clean up day 1 a bit.

--- paalel/1-depth/src/main.rs
+++ paalel/1-depth/src/main.rs
@@ -1,38 +1,26 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader, Error, ErrorKind};
 
-fn print_type_of<T>(_: &T) {
-    println!("type of input: {}", std::any::type_name::<T>())
-}
-
-// function takes path in the form of string slice and returns enum
-// which contains vector of integers on success or IO error type, see `std::io::Error`
 fn read_input(path: &str) -> Result<Vec<i64>, Error> {
-    let file = File::open(path)?; // open file by given path
-    // wrap file into generic buffered reader, it will use 4 KB buffer internally
-    // to reduce number of syscalls, thus improving performance
+    let file = File::open(path)?;
     let br = BufReader::new(file);
-    // create an empty vector, type of the stored elements will be inferred
     let mut v = Vec::new();
-    // br.lines() creates an iterator over lines in the reader
-    // see: https://doc.rust-lang.org/std/io/trait.BufRead.html#method.lines
+
     for line in br.lines() {
-        // IO operations generally can return error, we check if got
-        // an error,in which case we return this error as the function result
         let line = line?;
-        let n = line   
-            .trim() // trim "whitespaces"
-            .parse() // call `str::parse::<i64>(&self)` method on the trimmed line, which parses integer
-            .map_err(|e| Error::new(ErrorKind::InvalidData, e))?; // parse() can return error (e.g. for string "abc"), here if we got it, we convert it to `std::io::Error` type and return it as function result
+        let n = line
+            .trim()
+            .parse()
+            .map_err(|e| Error::new(ErrorKind::InvalidData, e))?;
+
         v.push(n); // push acquired integer to the vector
     }
-    Ok(v) // everything is Ok, return vector
+    Ok(v)
 }
 
-fn main() -> Result<(), Error>{
+fn main() -> Result<(), Error> {
     // Get input
     let depths = read_input("input.txt")?;
-    print_type_of(&depths);
 
     // Problem 1
     // Create windows [A, B] and check A < B
@@ -44,7 +32,10 @@ fn main() -> Result<(), Error>{
     // larger then the first.
     // Create windows [A, B, C, D] and check A < D
     increasing_depths = depths.windows(4).filter(|w| w[0] < w[3]).count();
-    println!("Number of increasing windowed depths is: {}", increasing_depths);
+    println!(
+        "Number of increasing windowed depths is: {}",
+        increasing_depths
+    );
 
     Ok(())
 }
