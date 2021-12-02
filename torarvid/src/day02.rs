da25cabf558ea5da0a45d95577b465c33d8348c7
Tor Arvid Lund <tor.arvid.lund@oda.com>
2021-12-02 11:04:41 +0100

    torarvid: day 2 cleanup (stolen FromStr from @erathe)

--- torarvid/src/day02.rs
+++ torarvid/src/day02.rs
@@ -1,3 +1,4 @@
+use std::str::FromStr;
 use crate::util::file_by_lines;
 
 enum Instruction {
@@ -6,27 +7,25 @@ enum Instruction {
     Forward(u64),
 }
 
-impl Instruction {
-    fn from_str(s: &str) -> Instruction {
-        if s.starts_with("down ") {
-            Instruction::Down(s[5..].parse().unwrap())
-        } else if s.starts_with("up ") {
-            Instruction::Up(s[3..].parse().unwrap())
-        } else {
-            Instruction::Forward(s[8..].parse().unwrap())
-        }
-    }
-}
+impl FromStr for Instruction {
+    type Err = String;
 
-impl From<&String> for Instruction {
-    fn from(s: &String) -> Instruction {
-        Instruction::from_str(s)
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.starts_with("down ") {
+            Ok(Instruction::Down(s[5..].parse().unwrap()))
+        } else if s.starts_with("up ") {
+            Ok(Instruction::Up(s[3..].parse().unwrap()))
+        } else if s.starts_with("forward ") {
+            Ok(Instruction::Forward(s[8..].parse().unwrap()))
+        } else {
+            Err(format!("Can't parse instruction {}", s))
+        }
     }
 }
 
 pub fn run() {
     let instructions: Vec<_> = file_by_lines("day02.txt").iter()
-        .map(|s| Instruction::from(s)).collect();
+        .map(|s| Instruction::from_str(s).unwrap()).collect();
 
     let mut depth = 0;
     let mut forward = 0;
