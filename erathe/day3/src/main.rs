38d33eb78ce5923520a4d7a6b182233d0fd4d39a
erathe <eirik.rathe@kolonial.no>
2021-12-03 09:48:58 +0100

    simpllify a tiny bit

--- erathe/day3/src/main.rs
+++ erathe/day3/src/main.rs
@@ -33,25 +33,21 @@ fn get_binary_value(lines: &Vec<&str>, pred: &dyn Fn(i32) -> bool) -> isize {
 
         let important_bit = if pred(bits) { b'1' } else { b'0' };
 
-        let new_lines = w_lines
-            .clone()
-            .into_iter()
-            .filter(|s| s.as_bytes()[i] == important_bit)
-            .collect_vec();
+        w_lines.retain(|&line| line.as_bytes()[i] == important_bit);
 
-        if new_lines.len() == 1 {
-            return isize::from_str_radix(&new_lines[0], 2).unwrap();
+        if w_lines.len() == 1 {
+            return isize::from_str_radix(&w_lines[0], 2).unwrap();
         }
-        w_lines = new_lines
     }
     0
 }
 
 fn part1(input: &str) -> isize {
     let len = input.lines().count();
-    let mut occs: [u32; 12] = [0; 12];
-    let mut gamma: [char; 12] = ['0'; 12];
-    let mut epsilon: [char; 12] = ['0'; 12];
+    let arr_len = input.lines().last().unwrap().len();
+    let mut occs: Vec<u32> = vec![0; arr_len];
+    let mut gamma: Vec<char> = vec!['0'; arr_len];
+    let mut epsilon: Vec<char> = vec!['0'; arr_len];
 
     input.lines().for_each(|b_str| {
         for (idx, c) in b_str
@@ -66,7 +62,7 @@ fn part1(input: &str) -> isize {
         }
     });
 
-    for n in 0..12 {
+    for n in 0..arr_len {
         if occs[n] > len as u32 / 2 {
             gamma[n] = '1';
         } else {
