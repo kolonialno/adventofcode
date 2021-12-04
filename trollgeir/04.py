ac474023a24644d204be32b4f4b74eaa152f5711
Asgeir Berland <asgeir.berland@oda.com>
2021-12-04 18:08:39 +0100

    🐼  -> 💀

--- trollgeir/04.py
+++ trollgeir/04.py
@@ -1,26 +1,25 @@
 from typing import List, Tuple
 
-import pandas as pd
-
-# Read and prepare data
-file = open("inputs/04.txt")
-draws = pd.Series(file.readline().rstrip().split(",")).astype(int)
-board_data = pd.read_csv(file, sep=r"\s+", header=None, skip_blank_lines=True)
-
-boards: List[pd.DataFrame] = []
-board_dimension = 5
-
-# Slice each board into separate frames
-for start_index in range(0, len(board_data), board_dimension):
-    boards.append(board_data.iloc[start_index : start_index + board_dimension, :])
+import numpy as np
 
 
-def run_bingo(draws: pd.Series, boards: List[pd.DataFrame]) -> List[Tuple[int, int]]:
+def read_input(filename) -> Tuple[np.ndarray, List[np.ndarray]]:
+    with open(filename) as f:
+        draws = np.array(f.readline().rstrip().split(",")).astype(int)
+        boards = [
+            np.array(
+                [[n for n in line.strip().split()] for line in board.split("\n")]
+            ).astype(int)
+            for board in f.read().strip().split("\n\n")
+        ]
+        return draws, boards
+
+
+def run_bingo(draws: np.ndarray, boards: List[np.ndarray]) -> List[Tuple[int, int]]:
     winner_sequence: List[Tuple[int, int]] = []  # (index, score)
     board_indexes = set(range(len(boards)))
 
-    # No point in checking for bingo too early
-    for draw_index in draws.index[len(boards[0]) :]:
+    for draw_index, _ in enumerate(draws):
         draws_so_far = draws[:draw_index]
 
         winners = set([i[0] for i in winner_sequence])
@@ -28,23 +27,16 @@ def run_bingo(draws: pd.Series, boards: List[pd.DataFrame]) -> List[Tuple[int, i
 
         for board_index in remaining_boards:
             board = boards[board_index]
-            for i in range(len(board)):
-                if (
-                    board.iloc[i].isin(draws_so_far).all()
-                    or board.T.iloc[i].isin(draws_so_far).all()
-                ):
-                    # Bingo!
-                    board_stack = board.stack()
-                    score = (
-                        board_stack[~board_stack.isin(draws_so_far)].sum()
-                        * draws_so_far.iloc[-1]
-                    )
-                    winner_sequence.append((board_index, score))
-                    break
+            hit_matrix = np.isin(board, draws_so_far)
+            if hit_matrix.all(axis=0).any() or hit_matrix.all(axis=1).any():
+                # Bingo!
+                score = board[~hit_matrix].sum() * draws_so_far[-1]
+                winner_sequence.append((board_index, score))
+
     return winner_sequence
 
 
-bingo_session_result = run_bingo(draws, boards)
+bingo_session_result = run_bingo(*read_input("inputs/04.txt"))
 
 answer1 = bingo_session_result[0][1]  # First winner score
 answer2 = bingo_session_result[-1][1]  # Last winner score
