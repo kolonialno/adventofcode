26fdcf0d2526f5f7ec7c656526eef818801716c1
Erik Haugan <209444+nnerik@users.noreply.github.com>
2021-12-05 13:03:50 +0100

    Nit: a tiny tad more Pythonic

--- nnerik/05.py
+++ nnerik/05.py
@@ -12,17 +12,17 @@ def count_overlaps(lines):
     for x1, y1, x2, y2 in lines:
         dx = 1 if x1 < x2 else -1 if x1 > x2 else 0
         dy = 1 if y1 < y2 else -1 if y1 > y2 else 0
-        for i in range(max(abs(x1 - x2), abs(y1 - y2)) + 1):
+        for i in range(max(x1 - x2, x2 - x1, y1 - y2, y2 - y1) + 1):
             seafloor[(x1 + i * dx, y1 + i * dy)] += 1
 
-    return sum(overlaps > 1 for overlaps in seafloor.values())
+    return sum(1 for n in seafloor.values() if n > 1)
 
 
 if __name__ == "__main__":
     input = read_input(__file__[:-3] + ".txt")
 
     # Part 1
-    result = count_overlaps(filter(lambda l: l[0] == l[2] or l[1] == l[3], input))
+    result = count_overlaps(l for l in input if l[0] == l[2] or l[1] == l[3])
     print(f"Part 1: {result}")
 
     # Part 2
