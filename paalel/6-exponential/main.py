e4b743effed462dd3f58b47a4ce613c86c6238e5
Pål Erik Lystad <pal.erik.lystad@oda.com>
2021-12-06 12:03:26 +0100

    Day 5 (python) -- compress a bit

--- paalel/6-exponential/main.py
+++ paalel/6-exponential/main.py
@@ -1,13 +1,9 @@
-def read_input(filename):
-    initial =[0] * 9
+def solve_jellies(filename, N=10):
     with open(filename, "r+") as file:
-        line = file.readline()
-        for n in line.rstrip().split(","):
-            initial[int(n)] += 1
+        jellies =[0] * 9
+        for n in file.readline().rstrip().split(","):
+            jellies[int(n)] += 1
 
-        return initial
-
-def solve_jellies(jellies, N=10):
     for _ in range(N):
         jellies.insert(8, jellies.pop(0))
         jellies[6] += jellies[8]
@@ -15,17 +11,9 @@ def solve_jellies(jellies, N=10):
     return sum(jellies)
 
 
-if __name__ == "__main__":
-    test_inital = read_input("test_input.txt")
-    assert solve_jellies(test_inital, N=18) == 26
-    test_inital = read_input("test_input.txt")
-    assert solve_jellies(test_inital, N=80) == 5934
+assert solve_jellies("test_input.txt", N=18) == 26
+assert solve_jellies("test_input.txt", N=80) == 5934
+assert solve_jellies("test_input.txt", N=256) == 26984457539
 
-    inital = read_input("input.txt")
-    print("Solution part 1:", solve_jellies(inital, N=80))
-
-    test_lines = read_input("test_input.txt")
-    assert solve_jellies(test_lines, N=256) == 26984457539
-
-    lines = read_input("input.txt")
-    print("Solution part 2:", solve_jellies(lines, N=256))
+print("Solution part 1:", solve_jellies("input.txt", N=80))
+print("Solution part 2:", solve_jellies("input.txt", N=256))
