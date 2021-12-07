ce438d939b7157e0491f701b264ee6f09e032a3a
Asgeir Berland <asgeir.berland@oda.com>
2021-12-07 14:34:15 +0100

    refactor a bit

--- trollgeir/07.py
+++ trollgeir/07.py
@@ -1,23 +1,23 @@
 import numpy as np
 
 with open("inputs/07.txt") as f:
-    initial_alignments = np.array([i for i in f.readline().split(",")]).astype(int)
+    initial_state = np.array([i for i in f.readline().split(",")]).astype(int)
 
 
-def calculate_fuel(initial_alignments, constant_rate=True):
-    dim1 = initial_alignments.max()
-    dim2 = len(initial_alignments)
+def calculate_fuel(initial_state, constant_rate=True):
+    dim1 = initial_state.max()
+    dim2 = len(initial_state)
     alignment_goals = np.tile(range(dim1), dim2).reshape(dim2, dim1).T
-    diff = abs(initial_alignments - alignment_goals).T
+    diff = abs(initial_state - alignment_goals)
     if not constant_rate:
-        fuel = sum(diff * (diff + 1) / 2)  # formula: n*(n+1)/2
+        fuel = (diff * (diff + 1) / 2).sum(axis=1) # formula: n*(n+1)/2
     else:
-        fuel = sum(diff)
+        fuel = diff.sum(axis=1)
     return fuel.min()
 
 
-answer1 = calculate_fuel(input)
-answer2 = calculate_fuel(input, constant_rate=False)
+answer1 = calculate_fuel(initial_state)
+answer2 = calculate_fuel(initial_state, constant_rate=False)
 
 print(f"Answer1: {answer1}")  # 348664
 print(f"Answer2: {answer2}")  # 100220525.0
