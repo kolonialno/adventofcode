5a74969036bdb5d30cbf2d3cb2f273fff64ffc75
Asgeir Berland <asgeir.berland@oda.com>
2021-12-07 14:11:03 +0100

    remove for loop and vectorize

--- trollgeir/07.py
+++ trollgeir/07.py
@@ -1,21 +1,19 @@
 import numpy as np
 
 with open("inputs/07.txt") as f:
-    input = [int(i) for i in f.readline().split(",")]
+    initial_alignments = np.array([i for i in f.readline().split(",")]).astype(int)
 
 
-def calculate_fuel(initial_alignment, constant_rate=True):
-    min_fuel = float("inf")
-    for i in input:
-        alignment_goal = np.array([i] * len(initial_alignment))
-        diff = abs(initial_alignment - alignment_goal)
-        if not constant_rate:
-            fuel = sum(diff * (diff + 1) / 2)  # formula: n*(n+1)/2
-        else:
-            fuel = sum(diff)
-        if fuel < min_fuel:
-            min_fuel = fuel
-    return min_fuel
+def calculate_fuel(initial_alignments, constant_rate=True):
+    dim1 = initial_alignments.max()
+    dim2 = len(initial_alignments)
+    alignment_goals = np.tile(range(dim1), dim2).reshape(dim2, dim1).T
+    diff = abs(initial_alignments - alignment_goals).T
+    if not constant_rate:
+        fuel = sum(diff * (diff + 1) / 2)  # formula: n*(n+1)/2
+    else:
+        fuel = sum(diff)
+    return fuel.min()
 
 
 answer1 = calculate_fuel(input)
