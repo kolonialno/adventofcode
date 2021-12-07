0e9e44d6f18afa6a63889fd85a3c11082b3c2b03
Tor Arvid Lund <tor.arvid.lund@oda.com>
2021-12-07 09:40:50 +0100

    torarvid: day 7 rust (cleanup nit stolen from @nnerik)

--- torarvid/src/day07.rs
+++ torarvid/src/day07.rs
@@ -6,13 +6,13 @@ pub fn run() {
         .split(",")
         .map(|x| x.parse::<isize>().unwrap())
         .collect::<Vec<isize>>();
-    let min_fuel = find_min_fuel(&positions, |i, j| (i - j).abs());
+    let min_fuel = find_min_fuel(&positions, |d| d);
     println!("Part 1: {}", min_fuel);
-    let min_fuel = find_min_fuel(&positions, |i, j| ((i - j).abs() * ((j - i).abs() + 1)) / 2);
+    let min_fuel = find_min_fuel(&positions, |d| d * (d + 1) / 2);
     println!("Part 2: {}", min_fuel);
 }
 
-fn find_min_fuel(positions: &Vec<isize>, diff_function: fn(i: isize, j: isize) -> isize) -> isize {
+fn find_min_fuel(positions: &Vec<isize>, diff_function: fn(diff: isize) -> isize) -> isize {
     let min_position = positions.iter().min().unwrap();
     let max_position = positions.iter().max().unwrap();
     let mut min_fuel: isize = isize::max_value();
@@ -20,7 +20,7 @@ fn find_min_fuel(positions: &Vec<isize>, diff_function: fn(i: isize, j: isize) -
     for i in *min_position..=*max_position {
         let diffs = positions
             .iter()
-            .map(|j| diff_function(i, *j))
+            .map(|j| diff_function((i - j).abs()))
             .sum::<isize>();
         if diffs < min_fuel {
             min_fuel = diffs;
