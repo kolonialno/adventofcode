b5ce00e695fbf1329004da9071f9a408c521f5ed
Erik Haugan <209444+nnerik@users.noreply.github.com>
2021-12-09 21:50:01 +0100

    Day 8 golf, save 2 bytes: open(0) reads from stdin

--- nnerik/08/golf.py
+++ nnerik/08/golf.py
@@ -2,5 +2,5 @@ d="abcefg cf acdeg acdfg bcdf abdfg abdefg acf abcdefg abcdfg"
 f=lambda p,t:sum(sorted({c:p.count(c)for c in"abcdefg"}[s]for s in t))
 k={f(d,t):str(n)for n,t in enumerate(d.split())}
 c=lambda p,o:"".join(k[f(p,t)]for t in o.split())
-r=[c(*l.split("|"))for l in open("i")]
+r=[c(*l.split("|"))for l in open(0)]
 print(sum(1 for o in r for d in o if d in"1478"),sum(int(n)for n in r))
