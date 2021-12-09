11655a5a2125e1ed103b7374731e30d21bd8fbe3
Erik Haugan <209444+nnerik@users.noreply.github.com>
2021-12-09 23:17:27 +0100

    Golfed day 8 down to 317 characters

--- nnerik/08/golf.py
+++ nnerik/08/golf.py
@@ -1,6 +1,5 @@
 d="abcefg cf acdeg acdfg bcdf abdfg abdefg acf abcdefg abcdfg"
-f=lambda p,t:sum(sorted({c:p.count(c)for c in"abcdefg"}[s]for s in t))
-k={f(d,t):str(n)for n,t in enumerate(d.split())}
-c=lambda p,o:"".join(k[f(p,t)]for t in o.split())
-r=[c(*l.split("|"))for l in open(0)]
-print(sum(1 for o in r for d in o if d in"1478"),sum(int(n)for n in r))
+f=lambda p,t:sum({c:p.count(c)for c in"abcdefg"}[s]for s in t)
+r=["".join({f(d,t):str(n)for n,t in enumerate(d.split())}[f(p,t)]for
+t in o.split())for l in open(0)for p,o in[l.split("|")]]
+print(sum(d in"1478"for o in r for d in o),sum(int(n)for n in r))
--- nnerik/08/solve.py
+++ nnerik/08/solve.py
@@ -1,12 +1,12 @@
 digits = "abcefg cf acdeg acdfg bcdf abdfg abdefg acf abcdefg abcdfg"
 fingerprint = lambda pattern, digit: sum(
-    sorted({c: pattern.count(c) for c in "abcdefg"}[segment] for segment in digit)
+    {c: pattern.count(c) for c in "abcdefg"}[segment] for segment in digit
 )
 key = {fingerprint(digits, digit): str(n) for n, digit in enumerate(digits.split())}
 decode = lambda pattern, output: "".join(
     key[fingerprint(pattern, digit)] for digit in output.split()
 )
-results = [decode(*line.split("|")) for line in open("input.txt").readlines()]
+results = [decode(*line.split("|")) for line in open("input.txt")]
 
 print("Part 1:", sum(1 for output in results for d in output if d in "1478"))
 print("Part 2:", sum(int(n) for n in results))
