f3529bcba4e33c2b1173f49a56171e154b53e394
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2021-12-10 08:43:43 +0100

    Clean up solution to day 10

--- jakobgm/day10.py
+++ jakobgm/day10.py
@@ -6,42 +6,32 @@ puzzle = [
     in Path("inputs/10.txt").read_text().strip().split("\n")
 ]
 
-openers = "[({<"
-closers = "])}>"
+openers = "([{<"
+closers = ")]}>"
 correct_opener = {closer: opener for closer, opener in zip(closers, openers)}
-correct_closer = {opener: closer for closer, opener in correct_opener.items()}
 syntax_error_scoring = {
-    ")": 3,
-    "]": 57,
-    "}": 1197,
-    ">": 25137,
-}
-autocompletion_scoring = {
-    ")": 1,
-    "]": 2,
-    "}": 3,
-    ">": 4,
+    closer: score
+    for closer, score
+    in zip(closers, (3, 57, 1197, 25137))
 }
 
 syntax_error_score = 0
 autocompletion_scores = []
 
-uncorrupted_lines = []
 for line in puzzle:
     stack = []
     for operator in line:
         if operator in openers:
             stack.append(operator)
-        elif operator in closers:
-            opener = stack.pop()
-            if not correct_opener[operator] == opener:
-                syntax_error_score += syntax_error_scoring[operator]
-                break
+            continue
+        elif stack.pop() != correct_opener[operator]:
+            syntax_error_score += syntax_error_scoring[operator]
+            break
     else:
         autocompletion_score = 0
-        for operator in reversed(stack):
+        for opener in reversed(stack):
             autocompletion_score *= 5
-            autocompletion_score += autocompletion_scoring[correct_closer[operator]]
+            autocompletion_score += openers.find(opener) + 1
         autocompletion_scores.append(autocompletion_score)
 
 print(syntax_error_score)
