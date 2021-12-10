6d648b820feb346526ca2b9c3e6ba2b71a33da9c
Pål Erik Lystad <pal.erik.lystad@oda.com>
2021-12-10 10:48:31 +0100

    Clean up day 10.
    Kinda liked the implementation so just removed uneccessary variables,
    code duplicaiton, and gave variables more sensible names.

--- paalel/10-syntax/main.py
+++ paalel/10-syntax/main.py
@@ -1,75 +1,35 @@
+from functools import reduce
 
-left = set(["(", "{" ,"[", "<"])
-right = set([")", "}" ,"]", ">"])
 left_to_right = { "(": ")", "{" :"}" ,"[" :"]", "<" : ">"}
 penalties = {")": 3, "]": 57, "}": 1197, ">": 25137}
 scores = {")": 1, "]": 2, "}": 3, ">": 4}
 
-def solve_1(filename):
-    return_value = 0
+def solve(filename):
+    corrupted = 0
+    incomplete = []
     with open(filename) as file:
         for line in file.readlines():
             stack = []
             for c in line.rstrip():
-                if c in left:
+                if c in ["(", "{" ,"[", "<"]:
                     stack.append(c)
                     continue
 
-                if c in right:
-                    t = stack.pop()
-
-                    if left_to_right[t] == c:
-                        continue
-
-                    else:
-                        return_value += penalties[c]
-                        break
-
-                raise ValueError(f"c={c} not in left or right set")
-
-
-    return return_value
-
-def calc_score(completion_string):
-    return_value = 0
-    for c in completion_string:
-        return_value = 5 * return_value + scores[c]
-    return return_value
-
-def solve_2(filename):
-    errors = []
-    with open(filename) as file:
-        for line in file.readlines():
-            stack = []
-            for c in line.rstrip():
-                if c in left:
-                    stack.append(c)
+                if left_to_right[stack.pop()] == c:
                     continue
 
-                if c in right:
-                    t = stack.pop()
+                else:
+                    corrupted += penalties[c]
+                    stack = []
+                    break
 
-                    if left_to_right[t] == c:
-                        continue
+            if stack:
+                incomplete.append(reduce(lambda s,r: 5*s + scores[r], [left_to_right[c] for c in stack[::-1]], 0))
 
-                    else:
-                        stack = []
-                        break
+    return corrupted, sorted(incomplete)[len(incomplete)//2]
 
-                raise ValueError(f"c={c} not in left or right set")
+assert solve("test_input.txt")[0] == 26397
+print("solution problem 1:", solve("input.txt")[0])
 
-            # examine stack
-            completion_string = [left_to_right[c] for c in stack[::-1]]
-            score = calc_score(completion_string)
-            if score > 0:
-                errors.append(score)
-
-
-    return sorted(errors)[len(errors)//2]
-
-
-assert solve_1("test_input.txt") == 26397
-print("solution problem 1:", solve_1("input.txt"))
-
-assert solve_2("test_input.txt") == 288957
-print("solution problem 2:", solve_2("input.txt"))
+assert solve("test_input.txt")[1] == 288957
+print("solution problem 2:", solve("input.txt")[1])
