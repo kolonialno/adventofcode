23fb2d5b014212e063b11300b8ddb0382ad1fca5
Erik Haugan <209444+nnerik@users.noreply.github.com>
2021-12-10 19:37:04 +0100

    Day 10 golf - 313 bytes

---
+++ nnerik/10/golf10.py
@@ -0,0 +1,7 @@
+def c(i):
+ a=b=0;f,*r=i;z="b([{<"
+ while(a==0)*r and r[0]in z:r,a,b=c(r)
+ return(r,a,5*b+z.index([f,"b"][a>0]))if a+(r==[])else(r[1:],0,b)*(0<ord(r[0])-ord(f)<3)or(r,[3,57,1197,25137][")]}>".index(r[0])],0)
+s=[c(l.strip())[1:]for l in open(0)]
+t=sorted(r for _,r in s if r)
+print(sum(r for r,_ in s),t[len(t)//2])
