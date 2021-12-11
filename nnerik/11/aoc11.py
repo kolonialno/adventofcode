7fd52b0fbd5f78b98d1ba172c610bb209629f8e8
Erik Haugan <209444+nnerik@users.noreply.github.com>
2021-12-11 14:39:47 +0100

    Day 11 in Python

---
+++ nnerik/11/aoc11.py
@@ -0,0 +1,23 @@
+with open("input.txt", "r") as f:
+    state = [int(n) for line in f for n in line.strip()]
+    count = step = 0
+    while True:
+        if step == 100:
+            print("Part 1:", count)
+        if sum(state) == 0:
+            print("Part 2:", step)
+            break
+        step += 1
+        state = [s + 1 for s in state]
+        while flashes := sum(state[i] > 9 for i in range(100)):
+            count += flashes
+            influx = [
+                sum(
+                    state[i + dx + dy] > 9
+                    for dx in (-1, 0, 1)
+                    for dy in (-10, 0, 10)
+                    if 0 <= i + dy < 100 and 0 <= i % 10 + dx < 10
+                )
+                for i in range(100)
+            ]
+            state = [s + delta if 0 < s < 10 else 0 for s, delta in zip(state, influx)]
