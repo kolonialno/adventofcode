b225d3571eef397a351bed206019532fbaae9909
Asgeir Berland <asgeir.berland@oda.com>
2021-12-12 15:58:07 +0100

    Simplify 12

--- trollgeir/12.py
+++ trollgeir/12.py
@@ -11,23 +11,20 @@ with open("inputs/12.txt") as f:
 
 
 def traverse(path, possible_connections, max_visits=1):
+    paths = []
+    small_cave_count = Counter(p for p in path if p.islower())
+    can_revisit = list(small_cave_count.values()).count(2) <= 1
     for c in possible_connections:
-        small_cave_count = Counter(p for p in path if p.islower())
-        if (
-            small_cave_count[c] >= max_visits
-            or list(small_cave_count.values()).count(2) > 1
-        ):
-            continue
-        elif c == "end":
-            paths.append(path + [c])
-        else:
-            traverse(path + [c], connections[c], max_visits)
+        if small_cave_count[c] < max_visits and can_revisit:
+            if c == "end":
+                paths += [path + [c]]
+            else:
+                paths += traverse(path + [c], connections[c], max_visits)
+    return paths
 
 
-paths = []
-traverse(["start"], connections["start"])
+paths = traverse(["start"], connections["start"])
 print(f"Answer1: {len(paths)}")
 
-paths = []
-traverse(["start"], connections["start"], max_visits=2)
+paths = traverse(["start"], connections["start"], max_visits=2)
 print(f"Answer2: {len(paths)}")
