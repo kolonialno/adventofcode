7343231ee5ee561f527311f263c5b080776d8e3b
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2021-12-13 00:29:09 +0100

    Solve day 12 task 2

--- jakobgm/day12.py
+++ jakobgm/day12.py
@@ -11,18 +11,21 @@ graph = Graph()
 graph.add_edges_from(edges)
 
 finished = []
-unfinished_paths = [["start", neighbour] for neighbour in graph.neighbors("start")]
+unfinished_paths = [(False, ["start", neighbour]) for neighbour in graph.neighbors("start")]
 
+task_b = True
 while unfinished_paths:
-    path = unfinished_paths.pop()
+    has_double_visit, path = unfinished_paths.pop()
     for neighbour in graph.neighbors(path[-1]):
         if neighbour == "start":
             continue
         elif neighbour == "end":
             finished.append(path.copy() + [neighbour])
         elif neighbour.upper() == str(neighbour):
-            unfinished_paths.append(path.copy() + [neighbour])
+            unfinished_paths.append((has_double_visit, path.copy() + [neighbour]))
         elif neighbour not in path:
-            unfinished_paths.append(path.copy() + [neighbour])
+            unfinished_paths.append((has_double_visit, path.copy() + [neighbour]))
+        elif not has_double_visit and task_b:
+            unfinished_paths.append((True, path.copy() + [neighbour]))
 
 print(len(finished))
