5fa93447fbdf3ff46c65da99149bf87f19f092d4
Erik Haugan <209444+nnerik@users.noreply.github.com>
2021-12-13 08:46:49 +0100

    Day 13 in Python

---
+++ nnerik/13/aoc13.py
@@ -0,0 +1,36 @@
+def fold(p, axis, n):
+    x, y = p
+    if axis == "x" and x < n or axis == "y" and y < n:
+        return (x, y)
+    if axis == "x":
+        return (2 * n - x, y)
+    if axis == "y":
+        return (x, 2 * n - y)
+
+
+points = set()
+with open("input.txt") as f:
+    while line := next(f).strip():
+        points.add(tuple(map(int, line.split(","))))
+    folds = [
+        (axis, int(n)) for line in f for axis, n in (line[11:].strip().split("="),)
+    ]
+
+# Part 1
+print("Part 1:", len({fold(p, *(folds[0])) for p in points}))
+
+# Part 2
+for axis, n in folds:
+    points = {fold(p, axis, n) for p in points}
+
+width = max(p[0] for p in points) + 1
+height = max(p[1] for p in points) + 1
+
+print("Part 2:")
+for y in range(height):
+    for x in range(width):
+        if (x, y) in points:
+            print("#", end="")
+        else:
+            print(" ", end="")
+    print()
