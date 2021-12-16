d9056da0c03bdf5acc4dbfa6dcba46099210767a
Mats Krüger Svensson <mats@matsemann.com>
2021-12-16 10:59:05 +0100

    Lunch = prettify

--- matsemann/Day16.kt
+++ matsemann/Day16.kt
@@ -4,118 +4,112 @@ import java.math.BigInteger
 
 data class Packet(val version: Int, val type: Int, val value: BigInteger, val subPackets: List<Packet> = listOf())
 
-fun parseValue(str: String) : Pair<String, BigInteger> {
-    var remaining = str
-    var bitString = ""
+class Parser(var bits: String) {
 
-    while(remaining.startsWith("1")) {
-        bitString += remaining.drop(1).take(4)
-        remaining = remaining.drop(5)
-    }
-    bitString += remaining.drop(1).take(4)
-    remaining = remaining.drop(5)
+    private fun take(nBits: Int) = bits.take(nBits).also { bits = bits.drop(nBits) }
 
-    return remaining to bitString.toBigInteger(2)
-}
+    private fun parseValue(): BigInteger {
+        var bitString = ""
 
-fun parseSubPackets(str: String, type: Int, length: Int): Pair<String, List<Packet>> {
-    var remaining = str
-    val packets = mutableListOf<Packet>()
-    if (type == 1) {
-        for(i in 1..length) {
-            parsePacket(remaining).let {(r, p) ->
-                remaining = r
-                packets += p
-            }
+        while (bits.startsWith("1")) {
+            bitString += take(5).takeLast(4)
         }
-    } else {
-        while ((str.length - remaining.length) < length) {
-            parsePacket(remaining).let {(r, p) ->
-                remaining = r
-                packets += p
-            }
-        }
+        bitString += take(5).takeLast(4)
+
+        return bitString.toBigInteger(2)
     }
 
-    return remaining to packets
-}
-
-fun parsePacket(str: String): Pair<String, Packet> {
-    var remaining = str
-    val version = remaining.take(3).toInt(2)
-    remaining = remaining.drop(3)
-
-    val id = remaining.take(3).toInt(2)
-    remaining = remaining.drop(3)
-
-    if (id == 4) {
-        val r = parseValue(remaining)
-        return r.first to Packet(version, id, r.second)
-    } else {
-        val lengthType = remaining.take(1).toInt(2)
-        remaining = remaining.drop(1)
-
-        val lengthValue: Int
-        if (lengthType == 0) {
-            lengthValue = remaining.take(15).toInt(2)
-            remaining = remaining.drop(15)
+    private fun parseSubPackets(type: Int, length: Int): List<Packet> {
+        val packets = mutableListOf<Packet>()
+        if (type == 1) {
+            for (i in 1..length) {
+                packets += parsePacket()
+            }
         } else {
-            lengthValue = remaining.take(11).toInt(2)
-            remaining = remaining.drop(11)
+            val originalLength = bits.length
+            while ((originalLength - bits.length) < length) {
+                packets += parsePacket()
+            }
         }
-        val r = parseSubPackets(remaining, lengthType, lengthValue)
-        return r.first to Packet(version, id, BigInteger.ZERO, r.second)
+
+        return packets
+    }
+
+    fun parsePacket(): Packet {
+        val version = take(3).toInt(2)
+        val id = take(3).toInt(2)
+
+        return if (id == 4) {
+            Packet(version, id, parseValue())
+        } else {
+            val lengthType = take(1).toInt(2)
+
+            val lengthValue: Int = if (lengthType == 0) {
+                take(15).toInt(2)
+            } else {
+                take(11).toInt(2)
+            }
+            Packet(version, id, BigInteger.ZERO, parseSubPackets(lengthType, lengthValue))
+        }
+
+    }
+
+    companion object {
+        fun fromLine(line: String) = line.map {
+            (it.digitToIntOrNull() ?: (it.code - 'A'.code + 10)).toString(2).padStart(4, '0')
+        }.joinToString("") { it }.let {
+            Parser(it)
+        }
     }
 
 }
+
+
+
 fun day16_1(lines: List<String>): Any {
-    val bits = lines.first().map {
-        (it.digitToIntOrNull() ?: (it.code - 'A'.code + 10)).toString(2).padStart(4, '0')
-    }.joinToString("") { it }
+    val parser = Parser.fromLine(lines.first())
+    val packet = parser.parsePacket()
 
     fun traverse(p: Packet): List<Packet> {
         return listOf(p) + p.subPackets.flatMap { traverse(it) }
     }
 
-    val p = parsePacket(bits)
-    val packets = traverse(p.second)
-
+    val packets = traverse(packet)
     return packets.sumOf { it.version }
 }
 
 
 fun day16_2(lines: List<String>): Any {
-    val bits = lines.first().map {
-        (it.digitToIntOrNull() ?: (it.code - 'A'.code + 10)).toString(2).padStart(4, '0')
-    }.joinToString("") { it }
+    val parser = Parser.fromLine(lines.first())
+    val packet = parser.parsePacket()
 
     fun calculate(p: Packet): BigInteger {
+        val subCalculations = p.subPackets.map { calculate(it) }
         when (p.type) {
-            0 -> return p.subPackets.sumOf { calculate(it) }
-            1 -> return p.subPackets.map { calculate(it) }.fold(BigInteger.ONE, BigInteger::times)
-            2 -> return p.subPackets.minOf { calculate(it) }
-            3 -> return p.subPackets.maxOf { calculate(it) }
+            0 -> return subCalculations.sumOf { it }
+            1 -> return subCalculations.fold(BigInteger.ONE, BigInteger::times)
+            2 -> return subCalculations.minOf { it }
+            3 -> return subCalculations.maxOf { it }
             4 -> return p.value
-            5 -> return p.subPackets.map { calculate(it) }.let {
+            5 -> return subCalculations.let {
                 if (it[0] > it[1]) BigInteger.ONE else BigInteger.ZERO
             }
-            6 -> return p.subPackets.map { calculate(it) }.let {
+            6 -> return subCalculations.let {
                 if (it[0] < it[1]) BigInteger.ONE else BigInteger.ZERO
             }
-            7 -> return p.subPackets.map { calculate(it) }.let {
+            7 -> return subCalculations.let {
                 if (it[0] == it[1]) BigInteger.ONE else BigInteger.ZERO
             }
             else -> throw Exception("wtf")
         }
     }
 
-    val p = parsePacket(bits)
-    return calculate(p.second)
+    return calculate(packet)
 }
 
 fun main() {
 //    run("1", fileName = "day16_ex.txt", func = ::day16_1)
-        run("1", fileName = "day16_1.txt", func = ::day16_1)
+    run("1", fileName = "day16_1.txt", func = ::day16_1)
 //    run("2", fileName = "day16_ex.txt", func = ::day16_2)
     run("2", fileName = "day16_1.txt", func = ::day16_2)
 }
