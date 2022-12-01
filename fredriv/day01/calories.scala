2e478d7fdfb79ed37af16be0d5579ffe5f5c0c16
Fredrik Vraalsen <22197+fredriv@users.noreply.github.com>
2022-12-01 11:06:25 +0100

    fredriv: simplify scala version

--- fredriv/day01/calories.scala
+++ fredriv/day01/calories.scala
@@ -1,17 +1,10 @@
 import scala.io.Source
 
-extension[T](list: List[T])
-    def splitBy(delim: T): List[List[T]] =
-        list match
-            case List() => List()
-            case x :: xs1 if x == delim => xs1.splitBy(delim) // discard delimiters
-            case x :: xs1 =>
-                val (ys, zs) = xs1 span (_ != delim)
-                (x :: ys) :: zs.splitBy(delim)
-
 @main def calories =
-    val lines = Source.fromFile("input.txt").getLines.toList
-    val caloriesPerElf = lines.splitBy("").map(_.map(_.toInt).sum)
+  val data = Source.fromFile("input.txt").getLines.mkString("\n")
+  val caloriesPerElf =
+    for calories <- data.split("\n\n")
+    yield calories.split("\n").map(_.toInt).sum
 
-    println(caloriesPerElf.max)
-    println(caloriesPerElf.sorted(Ordering.Int.reverse).take(3).sum)
+  println(caloriesPerElf.max)
+  println(caloriesPerElf.sorted(Ordering.Int.reverse).take(3).sum)
