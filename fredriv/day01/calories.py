75f315202240ff713a57fb367d17da9069cd232e
Fredrik Vraalsen <22197+fredriv@users.noreply.github.com>
2022-12-01 06:28:08 +0100

    fredriv: day 1 python

---
+++ fredriv/day01/calories.py
@@ -0,0 +1,22 @@
+import sys
+
+per_elf = []
+total = 0
+
+for line in sys.stdin:
+    line = line.strip()
+    if line == "":
+        per_elf.append(total)
+        total = 0
+    else:
+        total += int(line)
+
+per_elf.append(total) # Last one
+
+per_elf.sort()
+print(per_elf[-3:])
+
+sum = 0
+for calories in per_elf[-3:]:
+    sum += calories
+print(sum)
