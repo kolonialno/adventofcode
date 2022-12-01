ef8a4d1de0d2970aefa749dbcd4de5346210181e
Fredrik Vraalsen <fredrik@vraalsen.no>
2022-12-01 07:15:07 +0100

    fredriv: rewrote scala w/splitBy extension function

--- fredriv/day01/calories.scala
+++ fredriv/day01/calories.scala
@@ -1,16 +1,17 @@
 import scala.io.Source
 
-def group(xs: List[String]): List[List[String]] =
-    xs match
-        case List() => List()
-        case "" :: xs1 => group(xs1) // discard empty lines
-        case x :: xs1 =>
-            val (ys, zs) = xs1 span (!_.isEmpty)
-            (x :: ys) :: group(zs)
+extension[T](list: List[T])
+    def splitBy(delim: T): List[List[T]] =
+        list match
+            case List() => List()
+            case x :: xs1 if x == delim => xs1.splitBy(delim) // discard delimiters
+            case x :: xs1 =>
+                val (ys, zs) = xs1 span (_ != delim)
+                (x :: ys) :: zs.splitBy(delim)
 
 @main def calories =
     val lines = Source.fromFile("input.txt").getLines.toList
-    val caloriesPerElf = group(lines).map(_.map(_.toInt).sum)
+    val caloriesPerElf = lines.splitBy("").map(_.map(_.toInt).sum)
 
     println(caloriesPerElf.max)
     println(caloriesPerElf.sorted(Ordering.Int.reverse).take(3).sum)
