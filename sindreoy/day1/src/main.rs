8931f214ed4a3917eae8a0f6c1d6806ca6ad7078
Sindre Bakke Øyen <sindre.bakke.oyen@gmail.com>
2022-12-01 13:16:15 +0100

    Add solution to day 1, task 2

--- sindreoy/day1/src/main.rs
+++ sindreoy/day1/src/main.rs
@@ -2,9 +2,21 @@ use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
 fn main() -> io::Result<()> {
-    let elf_calories_vec: Vec<i32> = get_elf_calories();
-    let max_calories = elf_calories_vec.iter().max();
-    println!("{:?}", max_calories);
+    let mut elf_calories_vec: Vec<i32> = get_elf_calories();
+
+    elf_calories_vec.sort();
+
+    let length = elf_calories_vec.len();
+
+    let top1 = elf_calories_vec[length-1];
+    let top2 = elf_calories_vec[length-2];
+    let top3 = elf_calories_vec[length-3];
+    let sum_top3 = top1 + top2 + top3;
+
+    println!("{}", top1);
+    println!("{}", top2);
+    println!("{}", top3);
+    println!("{}", sum_top3);
 
     Ok(())
 }
@@ -22,7 +34,6 @@ fn get_elf_calories() -> Vec<i32> {
             total_calories += line_content.parse::<i32>().unwrap()
         } else {
             elf_calorie_vecs.push(total_calories);
-            println!("{}", total_calories);
             total_calories = 0;
         }
     }
