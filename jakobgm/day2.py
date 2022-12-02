ca5c1de2484d14f93e83fcb576534a4a8b306859
Jakob Gerhard Martinussen <10655778+JakobGM@users.noreply.github.com>
2022-12-02 07:14:35 +0100

    day2: Clean up code with Enum implementation

--- jakobgm/day2.py
+++ jakobgm/day2.py
@@ -1,84 +1,76 @@
+import enum
 from pathlib import Path
 
+
+class Outcome(enum.Enum):
+    WIN = 6
+    DRAW = 3
+    LOSE = 0
+
+    @classmethod
+    def from_(cls, string) -> "Outcome":
+        return {
+            "X": cls.LOSE,
+            "Y": cls.DRAW,
+            "Z": cls.WIN,
+        }[string]
+
+
+class Move(enum.Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSOR = 3
+
+    def play(self, other: "Move") -> Outcome:
+        if self.wins_against() == other:
+            return Outcome.WIN
+        elif self is other:
+            return Outcome.DRAW
+        else:
+            return Outcome.LOSE
+
+    def loses_against(self) -> "Move":
+        return {
+            self.ROCK: self.PAPER,
+            self.PAPER: self.SCISSOR,
+            self.SCISSOR: self.ROCK,
+        }[self]
+
+    def wins_against(self) -> "Move":
+        return {
+            self.ROCK: self.SCISSOR,
+            self.PAPER: self.ROCK,
+            self.SCISSOR: self.PAPER,
+        }[self]
+
+    @classmethod
+    def from_(cls, string) -> "Move":
+        return {
+            "A": cls.ROCK,
+            "B": cls.PAPER,
+            "C": cls.SCISSOR,
+            "X": cls.ROCK,
+            "Y": cls.PAPER,
+            "Z": cls.SCISSOR,
+        }[string]
+
+
 problem = Path("input/2.txt").read_text().strip()
-
-MAP = {
-    "A": "rock",
-    "B": "paper",
-    "C": "scissor",
-    "X": "rock",
-    "Y": "paper",
-    "Z": "scissor",
-}
-
-POINTS = {
-    "rock": 1,
-    "paper": 2,
-    "scissor": 3,
-    "lose": 0,
-    "draw": 3,
-    "win": 6,
-}
-
-
-def resolve(them, me):
-    if them == me:
-        return 3 + POINTS[me]
-    elif (
-        (them == "scissor" and me == "rock")
-        or (them == "paper" and me == "scissor")
-        or (them == "rock" and me == "paper")
-    ):
-        return 6 + POINTS[me]
-    else:
-        return POINTS[me]
-
-
-points = 0
+task_a = 0
+task_b = 0
 for round in problem.split("\n"):
-    them, me = round.split()
-    result = resolve(MAP[them], MAP[me])
-    points += result
+    first, second = round.split()
+    them, me = Move.from_(first), Move.from_(second)
+    task_a += me.value + me.play(them).value
 
-print(points)
-
-
-MAP = {
-    "A": "rock",
-    "B": "paper",
-    "C": "scissor",
-    "X": "lose",
-    "Y": "draw",
-    "Z": "win",
-}
-
-LOSE = {
-    "rock": "scissor",
-    "paper": "rock",
-    "scissor": "paper",
-}
-WIN = {
-    "rock": "paper",
-    "paper": "scissor",
-    "scissor": "rock",
-}
-
-
-def elaborate(them, outcome):
-    if outcome == "draw":
-        return POINTS[them] + POINTS[outcome]
-    elif outcome == "win":
-        return POINTS[WIN[them]] + POINTS[outcome]
-    elif outcome == "lose":
-        return POINTS[LOSE[them]] + POINTS[outcome]
+    outcome = Outcome.from_(second)
+    if outcome is Outcome.DRAW:
+        me = them
+    elif outcome is Outcome.WIN:
+        me = them.loses_against()
     else:
-        raise
+        me = them.wins_against()
+    task_b += me.value + outcome.value
 
-
-points = 0
-for round in problem.split("\n"):
-    them, me = round.split()
-    result = elaborate(MAP[them], MAP[me])
-    points += result
-
-print(points)
+print(f"{task_a = }")
+print(f"{task_b = }")
