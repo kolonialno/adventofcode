d1af283ee2e57a00e602beeaffdd2cc61ffa7a10
Anders Hennum <git@andehen.net>
2022-12-02 14:07:19 +0100

    Day 02 - "simplify"

--- andehen/02/02.py
+++ andehen/02/02.py
@@ -1,74 +1,24 @@
-with open("input.txt") as f:
-    games = [g.split(" ") for g in f.read().split("\n")][:-1]
-
-
-M = {
-    "A": "R",
-    "B": "P",
-    "C": "S",
-    "X": "R",
-    "Y": "P",
-    "Z": "S",
-}
-
-S = {
-    "R": 1,
-    "P": 2,
-    "S": 3
-}
-
-def score(P1, P2):
-    # Draw
-    if P1 == P2:
-        return 3 + S[P2]
-
-    if P1 == "R":
-        # Loss
-        if P2 != "P":
-            return S[P2]
-        # win
-        return 6 + S[P2]
-
-    if P1 == "P":
-        # Loss
-        if P2 != "S":
-            return S[P2]
-        # win
-        return 6 + S[P2]
-
-    if P1 == "S":
-        # Loss
-        if P2 != "R":
-            return S[P2]
-        # win
-        return 6 + S[P2]
+S = {"A": 1, "B": 2, "C": 3, "X": 1, "Y": 2, "Z": 3}
 
 
 def score_p1(p1, p2):
-    P1 = M[p1]
-    P2 = M[p2]
 
-    return score(P1, P2)
+    d = S[p2] - S[p1]
+    s = S[p2]
 
-
-# X loose
-# Y Draw
-# Z win
-
-A = {
-        "R": {"X": "S", "Y": "R", "Z": "P"},
-        "P": {"X": "R", "Y": "P", "Z": "S"},
-        "S": {"X": "P", "Y": "S", "Z": "R"},
-}
+    if d == 0: return 3 + s # draw
+    if d in [1, -2]: return 6 + s # win
+    return s # loose
 
 
-def score_p2(p1, p2):
-    P1 = M[p1]
-    P2 = A[P1][p2]
-    return score(P1, P2)
+def score_p2(p1, o):
+    if o == "X": return S[p1] + 2 - 3 * (1 % S[p1]) # loose
+    if o == "Y": return 3 + S[p1] # draw
+    return 6 + (S[p1] % 3 + 1) # win
 
-scores_p1 = [score_p1(p1, p2) for p1, p2 in games]
-print(sum(scores_p1))
 
-scores_p2 = [score_p2(p1, p2) for p1, p2 in games]
-print(sum(scores_p2))
+with open("input.txt") as f:
+    games = [g.split(" ") for g in f.read().split("\n")][:-1]
+
+print(sum([score_p1(p1, p2) for p1, p2 in games]))
+print(sum([score_p2(p1, o) for p1, o in games]))
