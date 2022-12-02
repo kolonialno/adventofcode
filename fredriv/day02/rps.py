99f5248847f9ac35e5791b8a9efb98209e52b5ef
Fredrik Vraalsen <22197+fredriv@users.noreply.github.com>
2022-12-02 14:08:14 +0100

    fredriv: simplify day 2

--- fredriv/day02/rps.py
+++ fredriv/day02/rps.py
@@ -1,22 +1,23 @@
 with open("input.txt") as f:
     data = f.read()
 
+rounds = [line.split(" ") for line in data.split("\n") if line != ""]
+
 def score(outcome: dict[int]) -> int:
-    rounds = [line.split(" ") for line in data.split("\n") if line != ""]
     return sum([outcome[opponent][me] for (opponent, me) in rounds])
 
-outcome = {
+predict_move = {
     'A': {'X': 3 + 1, 'Y': 6 + 2, 'Z': 0 + 3},
     'B': {'X': 0 + 1, 'Y': 3 + 2, 'Z': 6 + 3},
     'C': {'X': 6 + 1, 'Y': 0 + 2, 'Z': 3 + 3},
 }
 
-print(f"Part 1: {score(outcome)}")
+print(f"Part 1: {score(predict_move)}")
 
-outcome2 = {
+predict_win = {
     'A': {'X': 0 + 3, 'Y': 3 + 1, 'Z': 6 + 2},
     'B': {'X': 0 + 1, 'Y': 3 + 2, 'Z': 6 + 3},
     'C': {'X': 0 + 2, 'Y': 3 + 3, 'Z': 6 + 1},
 }
 
-print(f"Part 2: {score(outcome2)}")
+print(f"Part 2: {score(predict_win)}")
