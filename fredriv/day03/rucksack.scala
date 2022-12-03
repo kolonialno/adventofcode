de4dc2045777fbdbbdb1d4d08046fab586c2800b
Fredrik Vraalsen <fredrik@vraalsen.no>
2022-12-03 13:45:27 +0100

    cleaned up scala

--- fredriv/day03/rucksack.scala
+++ fredriv/day03/rucksack.scala
@@ -3,11 +3,13 @@ import scala.io.Source
 def priority(c: Char) =
   if c < 'a' then c - 'A' + 27 else c - 'a' + 1
 
+def containers(rucksack: String): List[String] =
+  rucksack.splitAt(rucksack.length / 2).toList
+
 def common(l: List[String]): Char =
   l.map(_.toSet).reduceLeft(_ intersect _).head
 
 @main def main =
   val rucksacks = Source.fromFile("input.txt").getLines.toList
-  val containers = rucksacks.map(r => r.splitAt(r.length / 2).toList)
-  println("Part 1: " + containers.map(common).map(priority).sum)
+  println("Part 1: " + rucksacks.map(containers).map(common).map(priority).sum)
   println("Part 2: " + rucksacks.grouped(3).map(common).map(priority).sum)
