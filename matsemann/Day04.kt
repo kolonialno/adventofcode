99963055fbfc16195d152196a73f024d3c810dba
Mats Krüger Svensson <mats.kruger.svensson@oda.com>
2022-12-04 07:42:23 +0100

    Day04.kt

---
+++ matsemann/Day04.kt
@@ -0,0 +1,39 @@
+package com.matsemann.adventofcode2022
+
+import com.matsemann.adventofcode2022.IntVec.Companion.toIntVec
+
+
+fun day04_1(lines: List<String>): Any {
+    return lines.map { it.split(",") }
+        .map { (first, second) ->
+            first.toIntVec("-") to second.toIntVec("-")
+        }.count { (first, second) ->
+            first.x >= second.x && first.y <= second.y
+                    || second.x >= first.x && second.y <= first.y
+        }
+}
+
+fun day04_2(lines: List<String>): Any {
+    return lines.map { it.split(",") }
+        .map { (first, second) ->
+            first.toIntVec("-") to second.toIntVec("-")
+        }.count { (first, second) ->
+            first.y >= second.x && first.x <= second.y
+        }
+}
+
+fun main() {
+
+//    run("1", fileName = "day04_ex.txt", func = ::day04_1)
+//    run("2", fileName = "day04_ex.txt", func = ::day04_2)
+
+//    run("1", fileName = "day04.txt", func = ::day04_1)
+    run("2", fileName = "day04.txt", func = ::day04_2)
+}
+
+/*
+OUTPUT
+======
+
+
+ */
