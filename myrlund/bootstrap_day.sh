620d49fcdfc7f95abad582624b786dd9f822e237
Jonas Myrlund <388633+myrlund@users.noreply.github.com>
2022-12-05 13:53:15 +0100

    Fix newline issue in bootstrap script.

--- myrlund/bootstrap_day.sh
+++ myrlund/bootstrap_day.sh
@@ -6,7 +6,8 @@ printf -v folder "ep%02g" $1
 data=$(curl -sf -b session=$SESSION_KEY https://adventofcode.com/2022/day/$1/input)
 cp -pr scaffold $folder
 sed -i "" "s/EPISODE/$folder/g" $folder/Cargo.toml
-echo $data > $folder/input.txt
+echo "$data" > $folder/input.txt
 
 open -a "Visual Studio Code" $folder
 echo GOGOGO
+
