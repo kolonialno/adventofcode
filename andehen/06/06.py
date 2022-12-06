8f7cfb0ca95dae0de0460d61850c0e12023d918f
Anders Hennum <git@andehen.net>
2022-12-06 12:09:26 +0100

    Day 6 - simplify

--- andehen/06/06.py
+++ andehen/06/06.py
@@ -1,15 +1,11 @@
 def get_marker(chars, n):
-
-    current = list(chars[0:(n-1)])
-    for i in range(n - 1, len(chars)):
-        if len(set(current)) == (n-1) and chars[i] not in current:
-            return i + 1
-        current.pop(0)
-        current.append(chars[i])
+    for i in range(len(chars) - n):
+        if len(set(chars[i : (i + n)])) == n:
+            return i + n
 
 
 with open("input.txt") as f:
     data = f.read()
 
-    print(get_marker(data, 4))
-    print(get_marker(data, 14))
+print(get_marker(data, 4))
+print(get_marker(data, 14))
