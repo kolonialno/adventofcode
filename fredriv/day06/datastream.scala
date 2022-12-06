a2df36e4e3e91fb24df86c0a6ed95482f76df8e9
Fredrik Vraalsen <22197+fredriv@users.noreply.github.com>
2022-12-06 06:26:11 +0100

    cleanup scala

--- fredriv/day06/datastream.scala
+++ fredriv/day06/datastream.scala
@@ -1,11 +1,7 @@
 import scala.io.Source
 
 def findUniquePos(data: String, len: Int): Int =
-  data.sliding(len, 1)
-    .zipWithIndex
-    .find((s, _) => s.toSet.size == len)
-    .map((_, i) => i + len)
-    .get
+  data.sliding(len).indexWhere(_.toSet.size == len) + len
 
 @main def main =
   val data = Source.fromFile("input.txt").getLines.toList.head
