6f2e12a088650bc6f663f411af193e28dc334083
Pål Erik Lystad <pal.erik.lystad@oda.com>
2022-12-06 11:45:38 +0100

    Clean up day 6

--- paalel/06-tuning/src/main.rs
+++ paalel/06-tuning/src/main.rs
@@ -1,42 +1,56 @@
 use std::collections::HashSet;
 
-fn find_marker(string: &str, window_size: usize) -> i32 {
+fn find_marker(string: &str, window_size: usize) -> usize {
     let chars: Vec<char> = string.chars().collect();
 
-    for i in 0..chars.len() - window_size {
+    for i in 0..(chars.len() - window_size) {
         let set: HashSet<&char> = chars[i..(i + window_size)].iter().collect();
         if set.len() == window_size {
-            return (i + window_size).try_into().unwrap();
+            return i + window_size;
         }
     }
 
-    -1
+    0
 }
+
 fn main() {
     println!("Hello, day 6!");
 
-    #[rustfmt::skip]
-    assert_eq!(find_marker(&"bvwbjplbgvbhsrlpgdmjqwftvncz".to_string(), 4), 5);
-    #[rustfmt::skip]
-    assert_eq!(find_marker(&"nppdvjthqldpwncqszvftbrmjlhg:".to_string(), 4), 6);
-    #[rustfmt::skip]
-    assert_eq!( find_marker(&"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".to_string(), 4), 10);
-    #[rustfmt::skip]
-    assert_eq!( find_marker(&"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".to_string(), 4), 11);
-
     let input = include_str!("../input.txt");
     println!("Solution to problem 1:{}", find_marker(&input, 4));
-
-    #[rustfmt::skip]
-    assert_eq!(find_marker(&"mjqjpqmgbljsphdztnvjfqwrcgsmlb".to_string(), 14),19);
-    #[rustfmt::skip]
-    assert_eq!(find_marker(&"bvwbjplbgvbhsrlpgdmjqwftvncz".to_string(), 14),23);
-    #[rustfmt::skip]
-    assert_eq!(find_marker(&"nppdvjthqldpwncqszvftbrmjlhg".to_string(), 14),23);
-    #[rustfmt::skip]
-    assert_eq!(find_marker(&"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".to_string(), 14),29);
-    #[rustfmt::skip]
-    assert_eq!(find_marker(&"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".to_string(), 14),26);
-
     println!("Solution to problem 2:{}", find_marker(&input, 14));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::find_marker;
+
+    #[test]
+    fn window_size_4() {
+        let test_cases: [(&str, usize); 4] = [
+            ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5),
+            ("nppdvjthqldpwncqszvftbrmjlhg:", 6),
+            ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10),
+            ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11),
+        ];
+
+        for (input, result) in test_cases {
+            assert_eq!(find_marker(&input, 4), result)
+        }
+    }
+
+    #[test]
+    fn window_size_14() {
+        let test_cases: [(&str, usize); 5] = [
+            ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19),
+            ("bvwbjplbgvbhsrlpgdmjqwftvncz", 23),
+            ("nppdvjthqldpwncqszvftbrmjlhg", 23),
+            ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29),
+            ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26),
+        ];
+
+        for (input, result) in test_cases {
+            assert_eq!(find_marker(&input, 14), result)
+        }
+    }
 }
