4ae0ed9599058e134dd5411f62892c2158da06a0
Sigurd Ljødal <544451+ljodal@users.noreply.github.com>
2022-12-07 12:58:48 +0100

    Day 7 - Done. The 🦀 memory model is confusing

--- ljodal/day-07/src/main.rs
+++ ljodal/day-07/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[derive(Debug)]
 enum Command<'a> {
     LS { output: Vec<&'a str> },
@@ -25,52 +27,126 @@ fn parse_commands(input: &str) -> Vec<Command> {
         .collect()
 }
 
-struct Dir<'a> {
-    parent: Option<Box<Dir<'a>>>,
-    name: &'a str,
-    dirs: Vec<Dir<'a>>,
-    files: Vec<(&'a str, usize)>,
+#[derive(Debug)]
+enum FS {
+    Dir(HashMap<String, FS>),
+    File(usize),
 }
 
-fn recreate_fs(commands: Vec<Command>) -> Dir {
-    let mut root = Dir {
-        parent: None,
-        name: "",
-        dirs: vec![],
-        files: vec![],
-    };
+impl FS {
+    fn insert(&mut self, path: Vec<String>, name: String, entry: FS) {
+        match self {
+            Self::Dir(entries) => {
+                if path.is_empty() {
+                    entries.insert(name, entry);
+                } else {
+                    let fs = entries.get_mut(&path[0]).expect("foo");
+                    fs.insert(path[1..path.len()].into(), name, entry);
+                }
+            }
+            _ => panic!("Found file"),
+        }
+    }
 
-    let mut current = &root;
+    fn size(&self) -> usize {
+        match self {
+            Self::File(size) => size.clone(),
+            Self::Dir(entries) => entries.values().map(|entry| entry.size()).sum(),
+        }
+    }
+
+    fn print(&self, name: String, indent: usize) {
+        match self {
+            Self::Dir(entries) => {
+                println!("{:indent$}- {name}: (dir)", "", indent = indent);
+                for (name, entry) in entries.iter() {
+                    entry.print(name.into(), indent + 4);
+                }
+            }
+            Self::File(size) => println!("{:indent$}- {name} ({size})", "", indent = indent),
+        };
+    }
+
+    /// Get the name and size of all directories
+    fn dirs(&self) -> Vec<&FS> {
+        let mut dirs: Vec<&FS> = vec![];
+
+        if let Self::Dir(entries) = self {
+            dirs.push(self);
+            for entry in entries.values() {
+                for fs in entry.dirs() {
+                    dirs.push(fs);
+                }
+            }
+        }
+
+        dirs
+    }
+}
+
+fn recreate_fs(input: &str) -> FS {
+    let commands = parse_commands(input);
+
+    let mut fs: FS = FS::Dir(HashMap::new());
+    let mut current_path: Vec<String> = vec![];
+
     for command in commands {
         match command {
-            Command::LS { output } => {}
+            Command::LS { output } => {
+                for line in output {
+                    let (a, name) = line.split_once(" ").expect("Invalid ls output");
+                    if a == "dir" {
+                        fs.insert(current_path.clone(), name.into(), FS::Dir(HashMap::new()));
+                    } else {
+                        let size = a.parse::<usize>().expect("Invalid size");
+                        fs.insert(current_path.clone(), name.into(), FS::File(size));
+                    }
+                }
+            }
             Command::CD { into } => {
                 if into == ".." {
-                    current = &current.parent.expect("Cannot CD beyond root");
+                    current_path.remove(current_path.len() - 1);
                 } else if into == "/" {
-                    current = &root;
+                    current_path = vec![];
                 } else {
-                    current = current
-                        .dirs
-                        .iter()
-                        .find(|dir| dir.name == into)
-                        .expect("cd into unknown dir");
+                    current_path.push(into.into());
                 }
             }
         }
     }
 
-    root
+    fs
 }
 
 fn solve_part1(input: &str) -> usize {
-    let commands = parse_commands(input);
-    println!("Commands: {commands:?}");
-    0
+    let fs = recreate_fs(input);
+    // fs.print("/".into(), 0);
+
+    let dirs: Vec<usize> = fs.dirs().iter().map(|dir| dir.size()).collect();
+    let limit: usize = 100000;
+    dirs.iter()
+        .filter(|size| size.clone().clone() < limit)
+        .sum()
 }
 
 fn solve_part2(input: &str) -> usize {
-    0
+    let fs = recreate_fs(input);
+
+    let disk_size: usize = 70000000;
+    let needed_space: usize = 30000000;
+    let currently_free = disk_size - fs.size();
+    let need_to_delete = needed_space - currently_free;
+
+    let mut dirs: Vec<&FS> = fs.dirs();
+    dirs.sort_by(|a, b| a.size().cmp(&b.size()));
+
+    let to_delete = dirs
+        .iter()
+        .skip_while(|dir| dir.size() < need_to_delete)
+        .next()
+        .expect("No directories large enough to delete");
+
+    to_delete.size()
 }
 
 fn main() {
@@ -88,12 +164,12 @@ mod tests {
     #[test]
     fn test_part1() {
         let input = include_str!("./test.txt");
-        assert_eq!(solve_part1(input), 95437);
+        assert_eq!(solve_part1(input), 95437, "Wrong result for pt. 1");
     }
 
     #[test]
     fn test_part2() {
         let input = include_str!("./test.txt");
-        assert_eq!(solve_part2(input), 4);
+        assert_eq!(solve_part2(input), 24933642, "Wrong result for pt. 2");
     }
 }
