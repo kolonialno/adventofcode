5e51b96e717307f08242ae7279b25c679465434a
Anders Hennum <git@andehen.net>
2022-12-07 13:47:35 +0100

    Use match-case

--- andehen/07/07_simple.py
+++ andehen/07/07_simple.py
@@ -1,23 +1,18 @@
 current = []
 visited = []
 
-
 with open("input.txt") as f:
-    for terminal_line in f.readlines():
-        if terminal_line.startswith("$ "):
-            if terminal_line.startswith("$ cd"):
-                cmd, arg = terminal_line[2:].strip().split(" ")
-                if arg == "..":
-                    visited.append(current.pop())
-                else:
-                    current.append(0)
-        else:
-            arg, name = terminal_line.strip().split(" ")
-            if arg.isdigit():
-                current = [d + int(arg) for d in current]
+    for line in f.readlines():
+        match line.split():
+            case ["$", "cd", ".."]:
+                visited.append(current.pop())
+            case ["$", "cd", arg]:
+                current.append(0)
+            case [size, name] if size.isdigit():
+                current = [d + int(size) for d in current]
 
 all = current + visited
-print(sum([s for s in all if s <= 100_000]))
+print(sum([size for size in all if size <= 100_000]))
 
 need_to_delete = 30_000_000 - (70_000_000 - all[0])
-print(sorted([s for s in all if s >= need_to_delete])[0])
+print(sorted([size for size in all if size >= need_to_delete])[0])
