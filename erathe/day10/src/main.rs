139ffb778b5051e6905f49d60ef091b53dc9b26c
erathe <eirik.rathe@kolonial.no>
2022-12-10 11:36:53 +0100

    make it easily extensible juuuuust in case

--- erathe/day10/src/main.rs
+++ erathe/day10/src/main.rs
@@ -3,50 +3,21 @@ const SCREEN_SIZE: usize = 40;
 fn main() {
     let input = include_str!("../input.txt");
     let mut screen = Screen::default();
-    let mut commands = input
-        .lines()
-        .map(|l| match l.as_bytes()[0] {
-            b'n' => Command::new_noop(),
-            b'a' => Command::new_addx(l.split_once(" ").unwrap().1.parse::<i32>().unwrap()),
-            _ => panic!("malformed input"),
-        })
-        .peekable();
+    let mut commands = input.lines().map(|l| match l.as_bytes()[0] {
+        b'n' => Command::new_noop(),
+        b'a' => Command::new_addx(l.strip_prefix("addx ").unwrap().parse::<i32>().unwrap()),
+        _ => panic!("malformed input"),
+    });
 
-    let mut res = 0;
-    loop {
-        // Enter cycle, check if we need to process a new command.
-        if screen.current_command.is_none() {
-            screen.current_command = commands.next();
-        }
-
-        // During cycle
-        // part 1 lol
-        match screen.current_cycle {
-            19 => res += 20 * screen.register_x,
-            59 => res += 60 * screen.register_x,
-            99 => res += 100 * screen.register_x,
-            139 => res += 140 * screen.register_x,
-            179 => res += 180 * screen.register_x,
-            219 => res += 220 * screen.register_x,
-            _ => (),
-        }
-
-        //part 2
-        screen.draw();
-
-        // "After" cycle
-        screen.process_command();
-
-        // check if we have any more commands to process
-        if commands.peek().is_none() {
-            break;
-        }
-        screen.current_cycle += 1;
+    while let Some(command) = commands.next() {
+        screen.process_command(command);
+        screen.execute_command();
     }
     // part 1
-    dbg!(res);
+    println!("part 1: {:?}", screen.temp_buffer);
 
     // part 2
+    println!("part 2:");
     for row in screen.display {
         println!("{:?}", row.iter().collect::<String>());
     }
@@ -54,25 +25,38 @@ fn main() {
 
 struct Screen {
     register_x: i32,
-    current_command: Option<Command>,
+    temp_buffer: i32,
+    current_command: Option<CommandType>,
     current_cycle: i32,
-    cycles_processed: i32,
+    rem_inst_cycles: i32,
     display: [[char; 40]; 6],
 }
 
 impl Screen {
-    fn process_command(&mut self) {
-        self.cycles_processed += 1;
+    fn process_command(&mut self, command: Command) {
+        self.rem_inst_cycles = command.cycles;
+        self.current_command = Some(command.c_type);
+    }
+
+    fn execute_command(&mut self) {
+        for _ in 0..self.rem_inst_cycles {
+            self.tick();
+        }
         if let Some(command) = &self.current_command {
-            if command.cycles == self.cycles_processed {
-                match command.c_type {
-                    CommandType::Addx(v) => self.register_x += v,
-                    CommandType::Noop => (),
-                }
-                self.current_command = None;
-                self.cycles_processed = 0;
+            match command {
+                CommandType::Addx(v) => self.register_x += v,
+                CommandType::Noop => (),
             }
-        };
+            self.current_command = None;
+        }
+    }
+
+    fn tick(&mut self) {
+        if (self.current_cycle + 21) % SCREEN_SIZE as i32 == 0 {
+            self.temp_buffer += (self.current_cycle + 1) * self.register_x;
+        }
+        self.draw();
+        self.current_cycle += 1;
     }
 
     fn draw(&mut self) {
@@ -81,8 +65,8 @@ impl Screen {
             .abs_diff(self.current_cycle % SCREEN_SIZE as i32)
             <= 1
         {
-            self.display[self.current_cycle as usize / SCREEN_SIZE]
-                [self.current_cycle as usize % SCREEN_SIZE] = '#';
+            self.display[(self.current_cycle as usize / SCREEN_SIZE)]
+                [(self.current_cycle as usize % SCREEN_SIZE)] = '█';
         }
     }
 }
@@ -91,10 +75,11 @@ impl Default for Screen {
     fn default() -> Self {
         Self {
             register_x: 1,
-            current_command: None,
-            cycles_processed: 0,
-            current_cycle: 0,
             display: [[' '; 40]; 6],
+            current_command: None,
+            current_cycle: 0,
+            rem_inst_cycles: 0,
+            temp_buffer: 0,
         }
     }
 }
