02052648067bc41bdaaf70fc19d8f83e9e802c98
Tor Arvid Lund <tor.arvid.lund@oda.com>
2022-12-10 08:09:10 +0100

    torarvid: Day10 tiny refactor

--- torarvid/day10.go
+++ torarvid/day10.go
@@ -5,20 +5,21 @@ import (
 	"strings"
 )
 
-func day10Draw(pixel, X int, frameBuf []byte) {
-	cmp := pixel % 40
-	if cmp == X-1 || cmp == X || cmp == X+1 {
-		frameBuf[pixel] = '#'
-	} else {
-		frameBuf[pixel] = ' '
-	}
-}
-
-func day10Solve(lines []string) (int, []byte) {
+func (a Advent) Day10() {
+	lines := fileByLines("inputs/day10.txt")
 	X := 1
 	cycle := 1
 	sigStrength := 0
 	frameBuf := make([]byte, 40*6)
+
+	draw := func(pixel int) {
+		cmp := pixel % 40
+		frameBuf[pixel] = ' '
+		if cmp == X-1 || cmp == X || cmp == X+1 {
+			frameBuf[pixel] = '#'
+		}
+	}
+
 	for _, line := range lines {
 		diff := 0
 		cycles := 1
@@ -27,7 +28,7 @@ func day10Solve(lines []string) (int, []byte) {
 			cycles = 2
 		}
 		for i := 0; i < cycles; i++ {
-			day10Draw(cycle-1, X, frameBuf)
+			draw(cycle - 1)
 			cycle++
 			if i == 1 {
 				X += diff
@@ -37,13 +38,7 @@ func day10Solve(lines []string) (int, []byte) {
 			}
 		}
 	}
-	return sigStrength, frameBuf
-}
 
-func (a Advent) Day10() {
-	lines := fileByLines("inputs/day10.txt")
-
-	sigStrength, frameBuf := day10Solve(lines)
 	fmt.Println("Part 1:", sigStrength)
 	fmt.Println("Part 2:")
 	for i := 0; i < 6; i++ {
