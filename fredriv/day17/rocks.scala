21ec07de240c1c29f47789597b847ab12356890c
Fredrik Vraalsen <22197+fredriv@users.noreply.github.com>
2022-12-19 05:37:02 +0100

    fredriv: day 17 part 2

--- fredriv/day17/rocks.scala
+++ fredriv/day17/rocks.scala
@@ -18,12 +18,10 @@ val rocks = Array(
 )
 
 
-def part1(jets: Array[Char], width: Int, startX: Int, startY: Int): Int =
+def solve(jets: Array[Char], maxRocks: Int, width: Int = 7, startX: Int = 2, startY: Int = 3): Int =
   val all = ArrayBuffer[Array[Char]]()
-  // for i <- 0 to 8 do all.append(Array.fill(width)(' '))
 
   def collision(rock: Array[String], x: Int, y: Int): Boolean =
-
     if x < 0 || x + rock(0).size > width || y < 0 then
       true // reached bottom or sides
     else
@@ -37,61 +35,68 @@ def part1(jets: Array[Char], width: Int, startX: Int, startY: Int): Int =
       overlaps.contains(true)
 
   def drawRock(rock: Array[String], x: Int, y: Int): Unit =
-    // add new rows to array
     for (row, ry) <- rock.reverse.zipWithIndex
         (c, cx) <- row.zipWithIndex
         if c == '#'
     do
       while y + ry >= all.size do
-        // println("Add row to drawing")
         all.append(Array.fill(width)(' '))
       all(y + ry)(x + cx) = '#'
 
-
-  println("Jets: " + jets.mkString)
-
   var j = 0
+  var last = 0
 
-  for i <- 0 until 2022 do
+  for i <- 0 until maxRocks do
     val rock = rocks(i % rocks.length)
     var y = all.size + startY
     var x = startX
-    // println(s"i=$i  j=$j    x=$x y=$y")
-    // println("Rock: " + rock.mkString("\n"))
 
     // move down
     var done = false
     while !done do
       // move left / right
       val jet = jets(j % jets.size)
-      // println(s"Jet: $jet")
       if jet == '<' && !collision(rock, x - 1, y) then
         x -= 1
-        // println(s"Moving left, x = $x")
       else if jet == '>' && !collision(rock, x + 1, y) then
         x += 1
-        // println(s"Moving right, x = $x")
 
       // move down
       if !collision(rock, x, y - 1) then
         y -= 1
-        // println(s"Moving down, y = $y")
       else
-        // println("Stopped")
         done = true
 
       j += 1 // next jet
 
     drawRock(rock, x, y)
 
-    // for r <- all.reverse do
-    //   println("|" + r.mkString + "|")
-    // println("+" + ("_" * width) + "+")
-
   all.size
 
 
-@main def main =
-  val input = Source.fromFile("input.txt").getLines.toList.head
+def part1(jets: Array[Char]): Int =
+  solve(jets, 2022)
 
-  println("Part 1: " + part1(input.toArray, 7, 2, 3))
+
+def part2(jets: Array[Char]): BigInt =
+  val goal = BigInt("1000000000000")
+  val repeatsAfter = 1745 * jets.size
+  val iters = goal / repeatsAfter
+  val initial = (goal % repeatsAfter).toInt
+
+  println(s"Computing initial $initial:")
+  val r1 = solve(jets, initial)
+  println(r1)
+
+  println(s"Computing after first repetition of $repeatsAfter:")
+  val r2 = solve(jets, initial + repeatsAfter) - r1
+  println(r2)
+
+  iters * r2 + r1
+
+
+@main def main =
+  val input = Source.fromFile("input.txt").getLines.toList.head.toArray
+
+  println("Part 1: " + part1(input))
+  println("Part 2: " + part2(input))
