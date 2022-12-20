8a7718adbb1f03648ca16835adfc89e08bd32fbe
Fredrik Vraalsen <22197+fredriv@users.noreply.github.com>
2022-12-20 10:10:28 +0100

    cleanup

--- fredriv/day20/grove.scala
+++ fredriv/day20/grove.scala
@@ -1,30 +1,21 @@
 import scala.io.Source
 
 def mix(numbers: Vector[(BigInt, Int)], startIndex: Int): Vector[(BigInt, Int)] =
-  val size = numbers.size
   val oldPos = numbers.indexWhere(_._2 == startIndex)
   val numberWithIndex: (BigInt, Int) = numbers(oldPos)
 
-  var newPos = ((oldPos + numberWithIndex._1) % (size - 1)).toInt
-  if newPos <= 0 then
-    newPos += size - 1
+  val size = numbers.size - 1 // first and last pos are the same in circular list
+  val pos = oldPos + numberWithIndex._1
+  val newPos = if pos > 0 then (pos % size).toInt else (pos % size + size).toInt
 
-  if newPos == oldPos then
-    numbers // no change
-  else if newPos < oldPos then
-    val start = numbers.take(newPos)
-    // new pos
+  val start = numbers.take(Math.min(newPos, oldPos))
+  val end = numbers.drop(Math.max(newPos, oldPos) + 1)
+  if newPos < oldPos then
     val middle = numbers.drop(newPos).take(oldPos - newPos)
-    // old pos
-    val end = numbers.drop(oldPos + 1)
     (start :+ numberWithIndex) ++ middle ++ end
-  else // oldPos < newPos
-    val start = numbers.take(oldPos)
-    // old pos
+  else
     val middle = numbers.drop(oldPos + 1).take(newPos - oldPos)
-    // new pos
-    val end = numbers.drop(newPos + 1)
-    start ++ (middle :+ numberWithIndex) ++ end
+    start ++ middle ++ (numberWithIndex +: end)
 
 
 def mix(numbers: Vector[(BigInt, Int)]): Vector[(BigInt, Int)] =
@@ -32,32 +23,25 @@ def mix(numbers: Vector[(BigInt, Int)]): Vector[(BigInt, Int)] =
 
 
 def score(mixed: Vector[(BigInt, Int)]): BigInt =
-  // println("Mixed: " + mixed.map(_._1).mkString(", "))
-  val size = mixed.size
-
   val pos = mixed.indexWhere(_._1 == 0)
-  val n1 = mixed((pos + 1000) % size)._1
-  val n2 = mixed((pos + 2000) % size)._1
-  val n3 = mixed((pos + 3000) % size)._1
-  println(s"$n1 + $n2 + $n3")
-  n1 + n2 + n3
+  val nums = for i <- List(1000, 2000, 3000) yield mixed((pos + i) % mixed.size)._1
+  println(nums.mkString(" + "))
+  nums.sum
 
 
-def part1(numbers: Vector[Int]): BigInt =
-  val size = numbers.size
-  val mixed = mix(numbers.map(BigInt.apply).zipWithIndex)
+def part1(numbers: Vector[BigInt]): BigInt =
+  val mixed = mix(numbers.zipWithIndex)
   score(mixed)
 
 
-def part2(numbers: Vector[Int]): BigInt =
-  val size = numbers.size
-  val initial = numbers.map(_ * BigInt("811589153")).zipWithIndex
+def part2(numbers: Vector[BigInt]): BigInt =
+  val initial = numbers.map(_ * 811589153).zipWithIndex
   val mixed = (1 to 10).foldLeft(initial)((nums, _) => mix(nums))
   score(mixed)
 
 
 @main def main =
-  val input = Source.fromFile("input.txt").getLines.map(_.toInt).toVector
+  val input = Source.fromFile("input.txt").getLines.map(BigInt.apply).toVector
 
   println(part1(input))
   println(part2(input))
