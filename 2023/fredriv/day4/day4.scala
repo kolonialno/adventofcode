b452bc14df72c193c04828d62b74dae10d02f3ed
Fredrik Vraalsen <fredrik@vraalsen.no>
2023-12-04 08:07:36 +0100

    Day 4 Scala

---
+++ 2023/fredriv/day4/day4.scala
@@ -0,0 +1,34 @@
+import scala.io.Source
+import collection.mutable.ArrayBuffer
+
+case class Card(id: Int, numbers: Set[Int], winning: Set[Int]):
+    val wins = numbers.intersect(winning)
+    val score = if wins.isEmpty then 0 else Math.pow(2, wins.size - 1).toInt
+    override def toString = s"""Card $id: Wins=${wins.mkString(" ")}, Score=$score"""
+
+def findNums(s: String) = "\\d+".r.findAllIn(s).map(_.toInt).toSet
+
+def parse(lines: Seq[String]): Seq[Card] =
+    val pattern = raw"Card\s+(\d+): (.*) \| (.*)".r
+
+    for line <- lines
+        m <- pattern.findFirstMatchIn(line)
+        List(cardId, wins, nums) = m.subgroups
+    yield
+        Card(cardId.toInt, findNums(nums), findNums(wins))
+
+@main def scratchcards =
+    val input = Source.fromFile("input.txt").getLines.toVector
+    val cards = parse(input)
+    for card <- cards do println(card)
+
+    println(s"Part 1: " + cards.map(_.score).sum)
+
+    val numCards = ArrayBuffer.fill(cards.length)(1)
+    for (c, i) <- cards.zipWithIndex
+        j <- 1 to c.wins.size
+        if i + j < numCards.size
+    do
+        numCards(i+j) += numCards(i)
+
+    println(s"Part 2: " + numCards.sum)
