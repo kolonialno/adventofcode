292fc6864633186ed4accd4d78917c765ec92e18
Fredrik Vraalsen <fredrik@vraalsen.no>
2023-12-05 18:22:47 +0100

    Simplify day 5 part 1

--- 2023/fredriv/day5/day5.scala
+++ 2023/fredriv/day5/day5.scala
@@ -6,31 +6,29 @@ case class Range(start: Long, end: Long):
 
 case class Mapping(range: Range, diff: Long)
 
-case class Mappings(from: String, to: String, mappings: Seq[Mapping]):
+case class Translation(from: String, to: String, mappings: Seq[Mapping]):
     def translate(pos: Long) =
-        val result = mappings.find(m => m.range.contains(pos)).map(m => pos + m.diff).getOrElse(pos)
-        result
+        mappings.find(_.range contains pos).map(pos + _.diff) getOrElse pos
 
-def parse(sections: Seq[Seq[String]]): Seq[Mappings] =
-    for section <- sections
-        Array(from, _, to) = section.head.split(" ")(0).split("-")
+def parse(section: Seq[String]): Translation =
+    val Array(from, _, to) = section.head.split(" ")(0).split("-")
 
-        mappings = for m <- section.tail
-            Array(destStart, srcStart, length) = m.split(" ").map(_.toLong)
-            range = Range(srcStart, srcStart + length - 1)
-            diff = destStart - srcStart
-        yield Mapping(range, diff)
+    val mappings = for m <- section.tail
+        Array(destStart, srcStart, length) = m.split(" ").map(_.toLong)
+        range = Range(srcStart, srcStart + length - 1)
+        diff = destStart - srcStart
+    yield Mapping(range, diff)
 
-    yield Mappings(from, to, mappings.sortBy(m => m.range.start))
+    Translation(from, to, mappings.sortBy(_.range.start))
 
 
 @main def seeds =
     val sections = Source.fromFile("input.txt").getLines.mkString("\n").split("\n\n").map(_.split("\n").toVector).toVector
     val seeds = sections(0)(0).split(": ")(1).split(" ").map(_.toLong).toVector
 
-    val mappings = parse(sections.tail)
+    val translations = sections.tail.map(parse)
 
     val distances = for seed <- seeds yield
-        mappings.foldLeft(seed)((pos, m) => m.translate(pos))
+        translations.foldLeft(seed)((pos, m) => m.translate(pos))
 
     println("Part 1: " + distances.min)
