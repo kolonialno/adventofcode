b1ca28b8c88abdfd728ea21f15f692a423070e6f
Fredrik Vraalsen <22197+fredriv@users.noreply.github.com>
2023-12-07 14:22:19 +0100

    Day 7 part 2 Scala

--- 2023/fredriv/day7/day7.scala
+++ 2023/fredriv/day7/day7.scala
@@ -1,36 +1,45 @@
 import scala.io.Source
 
-case class Card(c: Char):
+case class Card(c: Char, jokers: Boolean = false):
     def strength = c match {
         case 'A' => 14
         case 'K' => 13
         case 'Q' => 12
-        case 'J' => 11
+        case 'J' => if jokers then 1 else 11
         case 'T' => 10
         case c => c - '0'
     }
 
 
-case class Hand(cards: Seq[Card], bid: Int) extends Ordered[Hand]:
-    override def toString = s"Hand(${cards.map(_.c).mkString}, $bid)"
+case class Hand(cards: Seq[Card], bid: Int, jokers: Boolean = false) extends Ordered[Hand]:
+    override def toString = s"Hand(${cards.map(_.c).mkString}, $bid${if jokers then ", jokers" else ""})"
 
-    def strength =
-        val numOfAKind = cards.groupBy(_.c).view.mapValues(_.size).values.toSeq
-        numOfAKind.max match {
+    lazy val strength =
+        val numJokers = if jokers then cards.count(_.c == 'J') else 0
+        val numOfAKind = cards
+            .filter(card => !jokers || card.c != 'J')
+            .groupBy(_.c)
+            .mapValues(_.size)
+            .values.toSeq
+
+        val max = if numOfAKind.isEmpty then numJokers else numOfAKind.max + numJokers
+        val numPairs = numOfAKind.count(_ == 2)
+        max match {
             case 5 => 7 // Five of a kind
             case 4 => 6 // Four of a kind
             case 3 =>
-                if numOfAKind.contains(2) then 5 // Full house
+                if numJokers == 1 && numPairs == 2 then 5 // Full house
+                else if numJokers == 0 && numPairs == 1 then 5 // Full house
                 else 4 // Three of a kind
             case 2 =>
-                if numOfAKind.filter(_ == 2).size == 2 then 3 // Two pairs
+                if numPairs == 2 then 3 // Two pairs
                 else 2 // One pair
             case 1 => 1 // High card
         }
 
     def compare(that: Hand) =
         if this.strength == that.strength then
-            this.cards.zip(that.cards).map(_.strength - _.strength).filterNot(_ == 0).head
+            this.cards.zip(that.cards).map(_.strength - _.strength).find(_ != 0).get
         else this.strength - that.strength
 
 
@@ -40,8 +49,17 @@ case class Hand(cards: Seq[Card], bid: Int) extends Ordered[Hand]:
     val hands =
         for s <- input
             Array(cards, bid) = s.split(" ")
-        yield Hand(cards.map(Card), bid.toInt)
+        yield Hand(cards.map(Card(_)), bid.toInt)
 
     val ranked = hands.sorted.zipWithIndex
     val scores = ranked.map((hand, idx) => hand.bid * (idx + 1))
     println("Part 1: " + scores.sum)
+
+    val handsWithJoker =
+        for s <- input
+            Array(cards, bid) = s.split(" ")
+        yield Hand(cards.map(Card(_, jokers = true)), bid.toInt, jokers = true)
+
+    val rankedWithJoker = handsWithJoker.sorted.zipWithIndex
+    val scoresWithJoker = rankedWithJoker.map((hand, idx) => (idx + 1, hand, hand.strength, hand.bid.toLong * (idx + 1)))
+    println("Part 2: " + scoresWithJoker.map(_._4).sum)
