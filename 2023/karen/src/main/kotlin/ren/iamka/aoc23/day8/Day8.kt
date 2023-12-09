c0c7bba5c028d1f22c024d6dec78517995b68550
Karen Schwane <karen.schwane@oda.com>
2023-12-09 13:33:32 +0100

    Revisit day 8

--- 2023/karen/src/main/kotlin/ren/iamka/aoc23/day8/Day8.kt
+++ 2023/karen/src/main/kotlin/ren/iamka/aoc23/day8/Day8.kt
@@ -1,40 +1,14 @@
 package ren.iamka.aoc23.day8
 
 import ren.iamka.aoc23.readLines
+import java.util.*
 
 fun main() {
     println(part1())
+    println(part2())
 }
 
-fun part1(): Int {
-    var steps = 0
-    "/day8/data.txt".readLines {
-        val lines = this.toList()
-        val instructions = lines.first()
-        val regex = Regex("[A-Z]{3}")
-        val map = lines.takeLast(lines.size - 2).associate {
-            val (key, left, right) = regex.findAll(it).map { it.value }.toList()
-            key to (left to right)
-        }.toSortedMap()
-
-        var currentValue = map.firstKey()
-        val directionsIterator =
-            generateSequence(0) { it + 1 }.map { instructions[it % instructions.length] }.iterator()
-        while (currentValue != "ZZZ") {
-            val direction = directionsIterator.next()
-            val (left, right) = map[currentValue]!!
-            currentValue = when (direction) {
-                'L' -> left
-                'R' -> right
-                else -> throw IllegalArgumentException("Invalid direction $direction")
-            }
-            steps++
-        }
-    }
-    return steps
-}
-
-/*fun part2(): Unit {
+fun parse(operation: (map: SortedMap<String, Pair<String, String>>, directions: Iterator<Char>) -> Long): Long {
     var lines: List<String> = emptyList()
     "/day8/data.txt".readLines {
         lines = this.toList()
@@ -46,38 +20,40 @@ fun part1(): Int {
         key to (left to right)
     }.toSortedMap()
 
-    var keys = map.keys.filter { it.endsWith("A") }
-    var steps = 0
-    val directionsIterator = generateSequence(0L) { it + 1 }.map { instructions[(it % instructions.length.toLong()).toInt()] }.iterator()
+    val directionsIterator =
+        generateSequence(0L) { it + 1 }.map { instructions[(it % instructions.length.toLong()).toInt()] }.iterator()
 
-    while(!keys.all { it.endsWith("Z") }){
-        val direction = directionsIterator.next()
-        keys = keys.map { nextKey(key = it, map, direction) }
-        steps++
-    }
-
-    println(steps)
+    return operation(map, directionsIterator)
 }
 
-fun getSteps(key: String, map: SortedMap<String, Pair<String, String>>, instructions: String): Int {
-    println("Launch $key")
-    var steps = 0
-    val directionsIterator = generateSequence(0) { it + 1 }.map { instructions[it % instructions.length] }.iterator()
-    var currentKey = key
+fun part1(): Long {
+    return parse { map, directions ->
+        var steps = 0L
+        var currentKey = map.firstKey()
 
-    while (!currentKey.endsWith("Z")) {
-        val direction = directionsIterator.next()
-        val (left, right) = map[currentKey]!!
-        currentKey = when (direction) {
-            'L' -> left
-            'R' -> right
-            else -> throw IllegalArgumentException("Invalid direction $direction")
+        while (currentKey != "ZZZ") {
+            val direction = directions.next()
+            currentKey = nextKey(key = currentKey, map = map, direction = direction)
+            steps++
         }
-        // println("go to $currentKey")
-        steps++
+        steps
     }
-    println("Key $key steps $steps")
-    return steps
+}
+
+fun part2(): Long {
+    return parse { map, directions ->
+        val keys = map.keys.filter { it.endsWith("A") }
+        findLCMOfListOfNumbers(keys.map { key ->
+            var newKey = key
+            var steps = 0L
+            while (!newKey.endsWith("Z")) {
+                val direction = directions.next()
+                newKey = nextKey(key = newKey, map = map, direction = direction)
+                steps++
+            }
+            steps
+        })
+    }
 }
 
 fun nextKey(key: String, map: SortedMap<String, Pair<String, String>>, direction: Char): String {
@@ -88,4 +64,25 @@ fun nextKey(key: String, map: SortedMap<String, Pair<String, String>>, direction
         else -> throw IllegalArgumentException("Invalid direction $direction")
     }
 }
-*/
+
+// source: internet 🌚
+fun findLCM(a: Long, b: Long): Long {
+    val larger = if (a > b) a else b
+    val maxLcm = a * b
+    var lcm = larger
+    while (lcm <= maxLcm) {
+        if (lcm % a == 0L && lcm % b == 0L) {
+            return lcm
+        }
+        lcm += larger
+    }
+    return maxLcm
+}
+
+fun findLCMOfListOfNumbers(numbers: List<Long>): Long {
+    var result = numbers[0]
+    for (i in 1 until numbers.size) {
+        result = findLCM(result, numbers[i])
+    }
+    return result
+}
