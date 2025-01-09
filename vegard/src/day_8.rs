6fb9b8bb70349f74a65b8b2eeae9dbe08aa7be58
Vegard Pedersen <44409957+vegarped@users.noreply.github.com>
2025-01-09 19:04:51 +0100

    Vegard day 8 - playing with the result type

--- vegard/src/day_8.rs
+++ vegard/src/day_8.rs
@@ -1,12 +1,9 @@
 use crate::utils::read_to_matrix;
 use itertools::Itertools;
-use std::{
-    array,
-    collections::{HashMap, HashSet},
-};
+use std::collections::HashSet;
 
 fn solve() {
-    let input_map: Vec<Vec<char>> = read_to_matrix("data/8_demo.txt");
+    let input_map: Vec<Vec<char>> = read_to_matrix("data/8.txt");
 
     // Get symbol coordinates
     let mut symbol_coordinates: Vec<(char, usize, usize)> = Vec::new();
@@ -24,6 +21,7 @@ fn solve() {
 
     // Find all antinodes (incl duplicates and out of boudns)
     let mut antinodes: Vec<(usize, usize)> = Vec::new();
+    let mut antinodes_2: Vec<(usize, usize)> = Vec::new();
     for symbol in unique_symbols {
         for pairs in symbol_coordinates
             .iter()
@@ -31,7 +29,8 @@ fn solve() {
             .combinations(2)
             .map(|x| ((x[0].1, x[0].2), (x[1].1, x[1].2)))
         {
-            antinodes.append(&mut find_antinodes(pairs.0, pairs.1))
+            antinodes.append(&mut find_antinodes(pairs.0, pairs.1));
+            antinodes_2.append(&mut find_antinodes_2(pairs.0, pairs.1, &input_map));
         }
     }
     // Validate antinodes
@@ -40,9 +39,14 @@ fn solve() {
         .filter(|node| (node.0 < input_map.len()) & (node.1 < input_map[0].len()))
         .copied()
         .collect();
+    let valid_antinodes_2: HashSet<(usize, usize)> = antinodes_2
+        .iter()
+        .filter(|node| (node.0 < input_map.len()) & (node.1 < input_map[0].len()))
+        .copied()
+        .collect();
 
-    println!("Antinodes: {:?}", valid_antinodes);
     println!("Num antinodes: {:?}", valid_antinodes.len());
+    println!("Num antinodes 2: {:?}", valid_antinodes_2.len());
 }
 
 fn find_antinodes(a: (usize, usize), b: (usize, usize)) -> Vec<(usize, usize)> {
@@ -56,6 +60,43 @@ fn find_antinodes(a: (usize, usize), b: (usize, usize)) -> Vec<(usize, usize)> {
 
     antinodes
 }
+
+fn find_antinodes_2(
+    a: (usize, usize),
+    b: (usize, usize),
+    input_map: &Vec<Vec<char>>,
+) -> Vec<(usize, usize)> {
+    let mut antinodes: Vec<(usize, usize)> = Vec::new();
+
+    for row in 0..input_map.len() {
+        if let Ok(col)= get_col(row, a, b) {
+            antinodes.push((row, col))
+        }
+    }
+
+    antinodes
+}
+
+fn get_col(row: usize, a: (usize, usize),b: (usize, usize)) -> Result<usize, &'static str> {
+    // Get the column that makes the (row,col) point in line with a and b if it
+    // is one the same line and within the bounds of the map
+    // Using algebra, we have col = (b.1-a.1)*(row-a.0)/(b.0-a.0) + a.1
+    let numerator = (b.1 as isize - a.1 as isize) * (row as isize - a.0 as isize);
+    let denominator = b.0 as isize - a.0 as isize;
+    if denominator == 0 {
+        return Err("Denominator is zero");
+    } else if numerator % denominator != 0 {
+        return Err("Numerator is not divisible by denominator");
+    } else {
+        let col =numerator / denominator + a.1 as isize;
+        if col < 0 {
+            return Err("Column is negative");
+        } else {
+            return Ok(col as usize);
+        }
+    }
+}
+
 
 pub fn __main__() {
     solve();
